70fb46fdba1a6d615d2e85490d98329d8eab642c
the1812 <the1812@outlook.com>
2018-07-16 22:55:20 +0800

    Add missing files

---
+++ utils/remove-ads.js
@@ -0,0 +1,21 @@
+(() =>
+{
+    return () =>
+    {
+        waitForQuery()(
+            () => $("#slide_ad"),
+            it => it.length > 0,
+            it => it.css("display", "none")
+        );
+        waitForQuery()(
+            () => $("#home_popularize"),
+            it => it.length > 0,
+            it => it.css("display", "none")
+        );
+        waitForQuery()(
+            () => $(".gg-floor-module"),
+            it => it.length > 0,
+            it => it.css("display", "none")
+        );
+    };
+})();
