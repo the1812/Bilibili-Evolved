ad8f0e70a700034de59d3c5610097dfcaee3cbb5
the1812 <the1812@outlook.com>
2018-07-19 23:58:49 +0800

    Fix bug in dark style

--- style/dark-styles.js
+++ style/dark-styles.js
@@ -3,7 +3,7 @@
     return (settings, resources) =>
     {
         $("#bilibili-new-style-dark").remove();
-        if (settings.useNewStyle)
+        if (settings.useNewStyle && settings.useDarkStyle)
         {
             const styles = resources.getStyle("darkStyle", "bilibili-new-style-dark");
             $("body").after(styles);
