b96966944f5c43364d1848a12ae1555ff98b7522
the1812 <the1812@outlook.com>
2018-07-19 20:54:53 +0800

    Add element dependencies

--- bilibili-evolved.preview.user.js
+++ bilibili-evolved.preview.user.js
@@ -260,11 +260,29 @@
                     if (settingsBox)
                     {
                         $("body").append(settingsBox);
+
                         $(".gui-settings-close path").attr("d", "M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z");
                         $(".gui-settings-ok path").attr("d", "M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z");
+
+                        const dependencies = {};
                         for (const key in settings)
                         {
-                            $(`input[type='checkbox'][data='${key}']`).prop("checked", settings[key]);
+                            $(`input[type='checkbox'][key='${key}']`).prop("checked", settings[key]);
+                            $(`input[dependencies]`).each((_, element) =>
+                            {
+                                dependencies[$(element).prop("key")] = $(element).prop("dependencies");
+                            });
+                            $(`input[type='checkbox']`).on("change", () =>
+                            {
+                                const self = $(this);
+                                for (const key in dependencies)
+                                {
+                                    if (dependencies[key].indexOf(self.prop("key")) !== -1)
+                                    {
+                                        $(`input[key='${key}']`).prop("disabled", self.prop("checked"));
+                                    }
+                                }
+                            });
                         }
                     }
                 }
