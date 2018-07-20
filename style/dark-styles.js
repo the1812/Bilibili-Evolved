316d7fe1a3c1fa11d83150ec3e826a8d6207f29c
the1812 <the1812@outlook.com>
2018-07-20 17:00:16 +0800

    Revert "Fix bug in dark style"
    This reverts commit ad8f0e70a700034de59d3c5610097dfcaee3cbb5.

--- style/dark-styles.js
+++ style/dark-styles.js
@@ -3,7 +3,7 @@
     return (settings, resources) =>
     {
         $("#bilibili-new-style-dark").remove();
-        if (settings.useNewStyle && settings.useDarkStyle)
+        if (settings.useNewStyle)
         {
             const styles = resources.getStyle("darkStyle", "bilibili-new-style-dark");
             $("body").after(styles);
