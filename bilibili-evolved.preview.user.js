93293bf141fb1c89038964945d55670c97cc964b
the1812 <the1812@outlook.com>
2018-07-24 22:41:56 +0800

    Add textbox contents in settings

--- bilibili-evolved.preview.user.js
+++ bilibili-evolved.preview.user.js
@@ -134,7 +134,11 @@
         {
             for (const key in settings)
             {
-                settings[key] = GM_getValue(key, settings[key]);
+                const value = GM_getValue(key, settings[key]);
+                if (value !== undefined)
+                {
+                    settings[key] = value;
+                }
             }
         }
         function saveSettings()
@@ -251,10 +255,46 @@
         {
             loadSettings();
             reload(resources);
+
+            // gui-settings.js
             const svgData = {
                 settings: "M12,15.5A3.5,3.5 0 0,1 8.5,12A3.5,3.5 0 0,1 12,8.5A3.5,3.5 0 0,1 15.5,12A3.5,3.5 0 0,1 12,15.5M19.43,12.97C19.47,12.65 19.5,12.33 19.5,12C19.5,11.67 19.47,11.34 19.43,11L21.54,9.37C21.73,9.22 21.78,8.95 21.66,8.73L19.66,5.27C19.54,5.05 19.27,4.96 19.05,5.05L16.56,6.05C16.04,5.66 15.5,5.32 14.87,5.07L14.5,2.42C14.46,2.18 14.25,2 14,2H10C9.75,2 9.54,2.18 9.5,2.42L9.13,5.07C8.5,5.32 7.96,5.66 7.44,6.05L4.95,5.05C4.73,4.96 4.46,5.05 4.34,5.27L2.34,8.73C2.21,8.95 2.27,9.22 2.46,9.37L4.57,11C4.53,11.34 4.5,11.67 4.5,12C4.5,12.33 4.53,12.65 4.57,12.97L2.46,14.63C2.27,14.78 2.21,15.05 2.34,15.27L4.34,18.73C4.46,18.95 4.73,19.03 4.95,18.95L7.44,17.94C7.96,18.34 8.5,18.68 9.13,18.93L9.5,21.58C9.54,21.82 9.75,22 10,22H14C14.25,22 14.46,21.82 14.5,21.58L14.87,18.93C15.5,18.67 16.04,18.34 16.56,17.94L19.05,18.95C19.27,19.03 19.54,18.95 19.66,18.73L21.66,15.27C21.78,15.05 21.73,14.78 21.54,14.63L19.43,12.97Z",
                 close: "M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z",
                 ok: "M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z"
+            };
+            const textValidate = {
+                "customStyleColor": text =>
+                {
+                    const match = text.match(/^#([0-9a-fA-F]{6}|[0-9a-fA-F]{3})$/);
+                    if (match)
+                    {
+                        if (text.length < 7)
+                        {
+                            return `#${text[0]}${text[0]}${text[1]}${text[1]}${text[2]}${text[2]}`;
+                        }
+                        else
+                        {
+                            return text;
+                        }
+                    }
+                    else
+                    {
+                        return settings.customStyleColor;
+                    }
+                },
+                "blurBackgroundOpacity": text =>
+                {
+                    const match = text.match(/^([-\+]?\d+)(\.\d+)?$/);
+                    if (match)
+                    {
+                        const value = parseFloat(text);
+                        if (value >= 0 && value <= 1)
+                        {
+                            return text;
+                        }
+                    }
+                    return settings.blurBackgroundOpacity;
+                }
             };
             waitForQuery()(
                 () => $(".gr__bilibili_com>body"),
@@ -278,6 +318,13 @@
                     const settingsBox = resources.data.guiSettingsDom;
                     if (settingsBox)
                     {
+                        const reloadGuiSettings = () =>
+                        {
+                            for (const key in settings)
+                            {
+                                $(`input[type='checkbox'][key='${key}']`).prop("checked", settings[key]);
+                            }
+                        };
                         $("body").append(settingsBox);
                         $(".gui-settings-header .gui-settings-close").on("click", () =>
                         {
@@ -285,10 +332,18 @@
                         });
                         $("button.save").on("click", () =>
                         {
-                            $("input[type='checkbox'][key]").each((_, element) =>
-                            {
-                                settings[$(element).attr("key")] = $(element).prop("checked");
-                            });
+                            $("input[type='checkbox'][key]")
+                                .each((_, element) =>
+                                {
+                                    settings[$(element).attr("key")] = $(element).prop("checked");
+                                });
+                            $("input[type='text'][key]")
+                                .each((_, element) =>
+                                {
+                                    const key = $(element).attr("key");
+                                    const value = $(element).val();
+                                    settings[key] = textValidate[key](value);
+                                });
                             saveSettings();
                             const svg = $(".gui-settings-footer svg.gui-settings-ok");
                             if (parseInt(svg.css("width")) === 0)
@@ -305,16 +360,17 @@
                                     });
                                 }, 3000);
                             }
+                            reloadGuiSettings();
                         });
 
                         $(".gui-settings-close path").attr("d", svgData.close);
                         $(".gui-settings-ok path").attr("d", svgData.ok);
                         $(".gui-settings svg path").attr("d", svgData.settings);
 
+                        reloadGuiSettings();
                         const dependencies = {};
                         for (const key in settings)
                         {
-                            $(`input[type='checkbox'][key='${key}']`).prop("checked", settings[key]);
                             $(`input[dependencies]`).each((_, element) =>
                             {
                                 dependencies[$(element).attr("key")] = $(element).attr("dependencies");
