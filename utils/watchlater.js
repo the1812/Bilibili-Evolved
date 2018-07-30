d146d4972a4f6e2d82b570ad7b37d1ee0528749c
the1812 <the1812@outlook.com>
2018-07-30 19:35:17 +0800

    Fix bug in watchlater

--- utils/watchlater.js
+++ utils/watchlater.js
@@ -30,6 +30,11 @@
             items => redirectLinks(items)
         );
         waitForQuery()(
+            () => $(".av-about>a"),
+            it => it.length > 0,
+            items => redirectLinks(items)
+        );
+        waitForQuery()(
             () => $("div.watch-later-m>ul>div>li>a"),
             it => it.length > 0,
             items => redirectLinks(items)
@@ -47,13 +52,17 @@
                 width: "auto"
             })
         );
-        if (document.URL.indexOf("watchlater") !== -1 && document.URL.match(/av[\d]+/))
-        {
-            const av = document.URL.match(/av[\d]+/)[0];
-            if (av)
+        waitForQuery()(
+            () => document.URL.match(/av[\d]+/),
+            it => it && document.URL.indexOf("watchlater") !== -1,
+            it =>
             {
-                document.URL.replace(`https://www.bilibili.com/${av}`);
+                const id = it[0];
+                if (id)
+                {
+                    window.location.replace(`https://www.bilibili.com/${id}`);
+                }
             }
-        }
+        );
     };
 })();
