95fe8f81ed1197ccac98b78fee53b8a2e0153f48
the1812 <the1812@outlook.com>
2018-08-01 17:21:42 +0800

    Delete waitForQuery function

--- bilibili-evolved.preview.user.js
+++ bilibili-evolved.preview.user.js
@@ -84,35 +84,6 @@
             new SpinQuery(query, it => it.length === count, action).start();
         }
     }
-    function waitForQuery()
-    {
-        const MaxRetry = 30;
-        let retry = 0;
-        const tryQuery = (query, condition, action, failed) =>
-        {
-            if (retry >= MaxRetry)
-            {
-                if (failed)
-                {
-                    failed();
-                }
-            }
-            else
-            {
-                const result = query();
-                if (condition(result))
-                {
-                    action(result);
-                }
-                else
-                {
-                    retry++;
-                    setTimeout(() => tryQuery(query, condition, action, failed), 500);
-                }
-            }
-        };
-        return tryQuery;
-    }
     function ajax(url, done)
     {
         const xhr = new XMLHttpRequest();
