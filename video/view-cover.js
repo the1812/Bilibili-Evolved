d15d31167355f329397da241d39df8a5f62ee434
the1812 <the1812@outlook.com>
2018-08-28 12:36:25 +0800

    Add view cover for live room

--- video/view-cover.js
+++ video/view-cover.js
@@ -2,27 +2,64 @@
 {
     return () =>
     {
-        SpinQuery.any(() => $("span.settings-category"), it =>
+        SpinQuery.any(() => $("span.settings-category"), settingsCategories =>
         {
-            const metaData = $("meta[itemprop='image']");
-            if (metaData.length > 0)
+            SpinQuery.any(() => $("meta[itemprop='image']"), metaData =>
             {
-                $(it.filter((_, e) => e.innerHTML === "视频"))
-                    .parent()
-                    .after(`
-                <li class="indent-center">
-                <button
-                    class="gui-settings-button"
-                    title="查看当前视频的封面"
-                    id="view-cover">
-                    查看封面
-                </button>
-                </li>`);
-                $("#view-cover").on("click", () =>
+                if (metaData.length > 0)
                 {
-                    open(metaData.prop("content"));
-                });
-            }
+                    $(settingsCategories
+                        .filter((_, e) => e.innerHTML === "视频"))
+                        .parent()
+                        .after(`
+                    <li class="indent-center">
+                    <button
+                        class="gui-settings-button"
+                        title="查看当前视频的封面"
+                        id="view-video-cover">
+                        查看封面
+                    </button>
+                    </li>`);
+                    $("#view-video-cover").on("click", () =>
+                    {
+                        open(metaData.prop("content"));
+                    });
+                }
+            });
+
+            SpinQuery.any(() => $(".header-info-ctnr .room-cover"), coverLink =>
+            {
+                const match = coverLink
+                    .attr("href")
+                    .match(/space\.bilibili\.com\/([\d]+)/);
+                if (match && match[1])
+                {
+                    const uid = match[1];
+                    const url = `https://api.live.bilibili.com/bili/getRoomInfo/${uid}`;
+                    downloadText(url, text =>
+                    {
+                        // remove the surrounding "(...);"
+                        const jsonText = text.slice(1, -2);
+                        const coverUrl = JSON.parse(jsonText).data.cover;
+                        $(settingsCategories
+                            .filter((_, e) => e.innerHTML === "直播间"))
+                            .parent()
+                            .after(`
+                            <li class="indent-center">
+                            <button
+                                class="gui-settings-button"
+                                title="查看当前直播的封面"
+                                id="view-live-cover">
+                                查看封面
+                            </button>
+                            </li>`);
+                        $("#view-live-cover").on("click", () =>
+                        {
+                            open(coverUrl);
+                        });
+                    });
+                }
+            });
         });
 
         return {
