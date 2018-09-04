fe424b77d5b471ef7b69947525673bd0a6e797c8
the1812 <the1812@outlook.com>
2018-09-04 21:27:20 +0800

    Improve accuracy of remove ads

--- utils/remove-promotions.js
+++ utils/remove-promotions.js
@@ -5,7 +5,16 @@
         resources.applyStyle("removeAdsStyle", "remove-promotions-style");
         SpinQuery.any(
             () => $(".gg-pic").parent("a"),
-            it => it.css("display", "none")
+            it =>
+            {
+                it.css("display", "none");
+                it.each((_, element) =>
+                {
+                    const index = $(element).index() + 1;
+                    const selector = $(element).parent().parent().find(`.pic li:nth-child(${index})`);
+                    $(selector).css("visibility", "hidden");
+                });
+            }
         );
         return {
             ajaxReload: false
