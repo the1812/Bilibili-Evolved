b96b3364f0aaee299be0eb97b74f5eb111c1cf35
the1812 <the1812@outlook.com>
2018-09-11 14:31:47 +0800

    Add remove video top mask feature

--- video/remove-top-mask.js
+++ video/remove-top-mask.js
@@ -2,12 +2,12 @@
 {
     return (_, resources) =>
     {
-        const id = "bilibili-live-watermark";
+        const id = "bilibili-video-top-mask";
         if ($(`#${id}`).length === 0)
         {
             resources.applyStyleFromText(`
             <style id='${id}'>
-                .bilibili-live-player-video-logo
+                .bilibili-player-video-top
                 {
                     display: none !important;
                 }
