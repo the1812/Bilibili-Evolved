4f5d8c5e326f8062cb2c490c84a5374cdd9dff52
the1812 <the1812@outlook.com>
2018-10-12 23:26:42 +0800

    Fix bug in gui settings

--- utils/gui-settings.js
+++ utils/gui-settings.js
@@ -130,40 +130,10 @@
                 const box = $(".predefined-colors");
                 box.toggleClass("opened");
             });
-
-            // const button = document.querySelector("button.save");
-            // const svg = document.querySelector(".gui-settings-footer svg.gui-settings-ok");
-            // const saveCompleteClass = "save-complete";
-            // const animationend = () =>
-            // {
-            //     svg.classList.remove(saveCompleteClass);
-            //     button.classList.remove(saveCompleteClass);
-            // };
-            // button.addEventListener("animationend", animationend);
-            // svg.addEventListener("animationend", animationend);
-            // button.addEventListener("click", () =>
-            // {
-            //     $("input[type='checkbox'][key]")
-            //         .each((_, element) =>
-            //         {
-            //             settings[$(element).attr("key")] = $(element).prop("checked");
-            //         });
-            //     $("input[type='text'][key]")
-            //         .each((_, element) =>
-            //         {
-            //             const key = $(element).attr("key");
-            //             const value = $(element).val();
-            //             settings[key] = textValidate[key](value);
-            //         });
-            //     saveSettings(settings);
-            //     if ([svg, button].every(it =>
-            //         !it.classList.contains(saveCompleteClass)))
-            //     {
-            //         button.classList.add(saveCompleteClass);
-            //         svg.classList.add(saveCompleteClass);
-            //     }
-            //     syncGui();
-            // });
+            onSettingsChange(settingsChange);
+        }
+        function listenSettingsChange()
+        {
             const saveChanges = () =>
             {
                 $("input[type='checkbox'][key]")
@@ -179,17 +149,11 @@
                         settings[key] = textValidate[key](value);
                     });
                 saveSettings(settings);
-                // if ([svg, button].every(it =>
-                //     !it.classList.contains(saveCompleteClass)))
-                // {
-                //     button.classList.add(saveCompleteClass);
-                //     svg.classList.add(saveCompleteClass);
-                // }
                 syncGui();
+                console.log("settings saved");
             };
             $("input[type='checkbox'][key]").on("blur", () => saveChanges());
             $("input[type='text'][key]").on("change", () => saveChanges());
-            onSettingsChange(settingsChange);
         }
         function fillSvgData()
         {
@@ -294,6 +258,7 @@
                     listenDependencies();
                     syncGui();
                     addPredefinedColors();
+                    listenSettingsChange();
                     if (settings.blurSettingsPanel)
                     {
                         addEffects();
