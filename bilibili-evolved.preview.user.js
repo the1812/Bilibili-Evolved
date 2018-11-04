eca9d212588f865fb1b3aa84fde7e140769a598d
the1812 <the1812@outlook.com>
2018-11-04 18:38:33 +0800

    Update version number

--- bilibili-evolved.preview.user.js
+++ bilibili-evolved.preview.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Bilibili Evolved (Preview)
-// @version      1.5.23
+// @version      1.5.24
 // @description  增强哔哩哔哩Web端体验(预览版分支): 修复界面瑕疵, 删除广告, 使用夜间模式浏览, 下载视频或视频封面, 以及增加对触屏设备的支持等.
 // @author       Grant Howard, Coulomb-G
 // @copyright    2018, Grant Howrad (https://github.com/the1812)
