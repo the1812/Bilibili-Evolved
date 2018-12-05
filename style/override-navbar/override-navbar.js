c0c1e92653a190592db4a628381ebf265f4ed8f2
the1812 <the1812@outlook.com>
2018-12-05 22:07:11 +0800

    Refactor override navbar

--- style/override-navbar/override-navbar.js
+++ style/override-navbar/override-navbar.js
@@ -3,7 +3,7 @@
     return (settings, resources) =>
     {
         new SpinQuery(
-            () => $(".head-content.bili-wrapper>div.search").not(".filter-item"),
+            () => $(".head-content.bili-wrapper>div.search:not(.filter-item)"),
             it => it.length > 0 && $(".nav-con.fr").filter((_, it) => it.innerText).length > 0,
             textBox =>
             {
