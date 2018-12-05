6cbbaf86c8467e3c6ad7913a2dbdcbb16f1905bf
the1812 <the1812@outlook.com>
2018-12-05 15:55:07 +0800

    Fix bugs in override navbar

--- style/override-navbar/override-navbar.js
+++ style/override-navbar/override-navbar.js
@@ -4,12 +4,13 @@
     {
         new SpinQuery(
             () => $(".head-content.bili-wrapper>div.search").not(".filter-item"),
-            it => it.length > 0 && $(".nav-con.fr").length > 0,
+            it => it.length > 0 && $(".nav-con.fr").filter((_, it) => it.innerText).length > 0,
             textBox =>
             {
+                const rightNav = $(".nav-con.fr").filter((_, it) => it.innerText);
                 textBox.detach()
-                    // insert after only "first" right-nav. There will be 2 in Edge...
-                    .insertAfter($(document.querySelector(".nav-con.fr")));
+                    // insert after only the "real" right-nav. There will be 2 in Edge...
+                    .insertAfter(rightNav);
             }
         ).start();
     };
