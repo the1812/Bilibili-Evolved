ec9c9ed998d777ffeaa7a2020b508571ee7c38fa
the1812 <the1812@outlook.com>
2018-12-07 21:02:25 +0800

    Fix bugs in preserve rank

--- style/override-navbar/override-navbar.js
+++ style/override-navbar/override-navbar.js
@@ -11,20 +11,26 @@
                 textBox.detach()
                     // insert after only the "real" right-nav. There will be 2 in Edge...
                     .insertAfter(rightNav);
-                if (settings.preserveRank)
-                {
-                    const searchForm = textBox.find(".searchform");
-                    searchForm.addClass("preserve-rank");
-                    searchForm.prepend(`
-                    <a  title="排行榜"
-                        class="icons-enabled"
-                        href="https://www.bilibili.com/ranking"
-                        target="_blank">
-                        <i class="icon-rank"></i>
-                    </a>
-                    `);
-                }
             }
         ).start();
+
+        if (settings.preserveRank)
+        {
+            SpinQuery.any(
+                () => $(".head-content.bili-wrapper>div.search:not(.filter-item) .searchform,.nav-con #nav_searchform"),
+                searchForm =>
+                {
+                    searchForm.addClass("preserve-rank");
+                    searchForm.prepend(`
+                        <a  title="排行榜"
+                            class="icons-enabled"
+                            href="https://www.bilibili.com/ranking"
+                            target="_blank">
+                            <i class="icon-rank"></i>
+                        </a>
+                    `);
+                }
+            );
+        }
     };
 })();
