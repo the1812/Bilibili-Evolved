735fd8699d2929b78a921f14e21bd5d0f1d8664a
the1812 <the1812@outlook.com>
2018-12-07 21:26:33 +0800

    Add default video quality

--- video/default-video-quality.js
+++ video/default-video-quality.js
@@ -4,30 +4,63 @@
     {
         (async () =>
         {
+            const qualities = [
+                {
+                    name: "1080P+/60",
+                    value: 116,
+                },
+                {
+                    name: "720P+/60",
+                    value: 74,
+                },
+                {
+                    name: "1080P",
+                    value: 80
+                },
+                {
+                    name: "720P",
+                    value: 64
+                },
+                {
+                    name: "480P",
+                    value: 32
+                },
+                {
+                    name: "360P",
+                    value: 15
+                },
+                {
+                    name: "自动",
+                    value: 0
+                },
+            ];
             const dropdown = await SpinQuery.any(
                 () => $(".gui-settings-dropdown:has(input[key=defaultVideoQuality])"));
             const list = dropdown.find("ul");
             const input = dropdown.find("input");
-            [
-                "1080P60",
-                "720P60",
-                "1080P+",
-                "720P+",
-                "1080P",
-                "720P",
-                "480P",
-                "360P",
-                "自动",
-            ].forEach(value =>
+            qualities.forEach(item =>
                 {
-                    $(`<li>${value}</li>`).appendTo(list)
+                    $(`<li>${item.name}</li>`).appendTo(list)
                         .on("click", () =>
                         {
-                            input.val(value).trigger("input").change();
+                            input.val(item.name).trigger("input").change();
                         });
                 });
 
-            // const qualityMenu = await SpinQuery.any()
+            const qualityItems = await SpinQuery.condition(
+                () => $(".bilibili-player-video-quality-menu .bui-select-list>li.bui-select-item"),
+                it => it.length > 0 && $("li.profile-info").length > 0
+            );
+            const [targetQuality] = qualities
+                .filter(it => it.name === settings.defaultVideoQuality)
+                .map(it => it.value);
+            qualityItems.each((_, it) =>
+            {
+                if (parseInt(it.getAttribute("data-value")) === targetQuality)
+                {
+                    it.click();
+                }
+            });
         })();
     };
 })();
