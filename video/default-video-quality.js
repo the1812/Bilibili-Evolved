9da6f2824133a48a5d2ff01c8d95b5d521f995a9
the1812 <the1812@outlook.com>
2018-12-07 21:48:33 +0800

    Bug fixes

--- video/default-video-quality.js
+++ video/default-video-quality.js
@@ -6,12 +6,12 @@
         {
             const qualities = [
                 {
-                    name: "1080P+/60",
+                    name: "1080P60",
                     value: 116,
                 },
                 {
-                    name: "720P+/60",
-                    value: 74,
+                    name: "1080P+",
+                    value: 112,
                 },
                 {
                     name: "1080P",
@@ -51,12 +51,22 @@
                 () => $(".bilibili-player-video-quality-menu .bui-select-list>li.bui-select-item"),
                 it => it.length > 0 && $("li.profile-info").length > 0
             );
+            const [availableHighestQualities] = qualityItems.toArray()
+                .map(it => it.getAttribute("data-value"))
+                .sort()
+                .reverse();
             const [targetQuality] = qualities
                 .filter(it => it.name === settings.defaultVideoQuality)
                 .map(it => it.value);
+            const [finalQuality] = qualities
+                .map(it => it.value)
+                .filter(it => it <= (targetQuality || availableHighestQualities))
+                .sort()
+                .reverse();
+
             qualityItems.each((_, it) =>
             {
-                if (parseInt(it.getAttribute("data-value")) === targetQuality)
+                if (parseInt(it.getAttribute("data-value")) === finalQuality)
                 {
                     it.click();
                 }
