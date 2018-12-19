0beb22e5ad5eebff64b87f129ded77641d6f43f9
the1812 <the1812@outlook.com>
2018-12-19 23:21:37 +0800

    Fix typo

--- bilibili-evolved.preview.user.js
+++ bilibili-evolved.preview.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Bilibili Evolved (Preview)
-// @version      1.6.16
+// @version      1.6.17
 // @description  增强哔哩哔哩Web端体验(预览版分支): 修复界面瑕疵, 删除广告, 使用夜间模式浏览, 下载视频或视频封面, 以及增加对触屏设备的支持等.
 // @author       Grant Howard, Coulomb-G
 // @copyright    2018, Grant Howrad (https://github.com/the1812)
@@ -267,7 +267,7 @@
                 ],
                 displayNames: {
                     useNewStyle: "样式调整",
-                    blurBackgroundOpacity: "顶栏(对横幅)不透明度",
+                    blurBackgroundOpacity: "顶栏(对横幅)透明度",
                 },
             },
             overrideNavBar: {
