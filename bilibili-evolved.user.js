a9884f7250b8647461d33a1e58599d31cfb029c3
the1812 <the1812@outlook.com>
2019-01-06 12:38:50 +0800

    Reduce version number

--- bilibili-evolved.user.js
+++ bilibili-evolved.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Bilibili Evolved
-// @version      1.7.0
+// @version      1.6.29
 // @description  增强哔哩哔哩Web端体验: 修复界面瑕疵, 删除广告, 使用夜间模式浏览; 下载视频,封面,弹幕, 以及增加对触屏设备的支持等.
 // @author       Grant Howard, Coulomb-G
 // @copyright    2018, Grant Howrad (https://github.com/the1812)
