18280369002674fc16367646c6e7c2867f1eeae4
the1812 <the1812@outlook.com>
2019-01-07 12:21:27 +0800

    Add xml danmaku

--- video/danmaku/danmaku.js
+++ video/danmaku/danmaku.js
@@ -2,6 +2,23 @@
 {
     return (settings, resources) =>
     {
-
+        class XmlDanmaku
+        {
+            constructor(element)
+            {
+                this.pData = element.getAttribute("p");
+                [this.time, this.type, this.fontSize, this.color] = this.pData.split(",");
+                this.content = element.innerText;
+            }
+        }
+        class XmlDanmakuDocument
+        {
+            constructor(xml)
+            {
+                this.xml = xml;
+                const document = new DOMParser().parseFromString(xml, "application/xml").documentElement;
+                this.danmakus = [...document.querySelectorAll("d[p]")].map(it => new XmlDanmaku(it));
+            }
+        }
     };
 })();
