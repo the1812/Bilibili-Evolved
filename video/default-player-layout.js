15d354fd44de75120236d14d659bc83aea13cf44
the1812 <the1812@outlook.com>
2019-01-20 16:39:19 +0800

    Fix bugs

--- video/default-player-layout.js
+++ video/default-player-layout.js
@@ -42,7 +42,11 @@
             }
             class VideoLayoutCookie extends LayoutCookie
             {
-                static get cookieKey() { return "stardustvideo"; }
+                constructor()
+                {
+                    super();
+                    this.cookieKey = "stardustvideo";
+                }
                 useNewLayout()
                 {
                     this.setCookie(this.cookieKey, 1);
@@ -54,7 +58,11 @@
             }
             class BangumiLayoutCookie extends LayoutCookie
             {
-                static get cookieKey() { return "stardustpgcv"; }
+                constructor()
+                {
+                    super();
+                    this.cookieKey = "stardustpgcv";
+                }
                 useNewLayout()
                 {
                     this.setCookie(this.cookieKey, "0606");
@@ -64,10 +72,6 @@
                     this.clearCookie(this.cookieKey);
                 }
             }
-            // const isNewLayout = document.cookie.split(";")
-            //     .filter(it => it.includes(`${VideoLayoutCookie.cookieKey}=1`)
-            //         || it.includes(BangumiLayoutCookie.cookieKey)).length > 0
-            //     ? false : true;
 
             const cookies = LayoutCookie.getAll();
             $(dropdown).on("change", () =>
