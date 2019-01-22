9d6c928d03a60807bbeaa00613ce0afcc9adc2e8
the1812 <the1812@outlook.com>
2019-01-22 14:39:16 +0800

    Restore link

--- utils/show-dead-video-title.js
+++ utils/show-dead-video-title.js
@@ -8,8 +8,16 @@
             deadVideos.each((_, it) =>
             {
                 const $it = $(it);
+                const aid = $it.attr("data-aid");
                 const title = $it.find("img").attr("alt");
-                $it.find("a.title").attr("title", title).text(title);
+                $it.find("a.cover")
+                    .attr("target", "_blank")
+                    .attr("href", `//www.bilibili.com/video/av${aid}`);
+                $it.find("a.title")
+                    .attr("title", title)
+                    .attr("target", "_blank")
+                    .attr("href", `//www.bilibili.com/video/av${aid}`)
+                    .text(title);
             });
         }
         SpinQuery.any(() => $(".fav-content"), () =>
