48652daec1284276ea1123c7161a98508bad4b80
the1812 <the1812@outlook.com>
2019-03-02 20:27:06 +0800

    Fix unsupported API

--- bilibili-evolved.user.js
+++ bilibili-evolved.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Bilibili Evolved
-// @version      1.7.10
+// @version      1.7.11
 // @description  增强哔哩哔哩Web端体验: 下载视频, 封面, 弹幕; 自定义播放器的画质, 模式, 布局; 删除广告, 使用夜间模式, 修复界面瑕疵; 以及增加对触屏设备的支持等.
 // @author       Grant Howard, Coulomb-G
 // @copyright    2019, Grant Howard (https://github.com/the1812) & Coulomb-G (https://github.com/Coulomb-G)
@@ -1580,6 +1580,14 @@ class ResourceManager
         styles.push("--custom-control-background-opacity:" + settings.customControlBackgroundOpacity);
         this.applyStyleFromText(`<style id="bilibili-evolved-variables">html{${styles.join(";")}}</style>`);
     }
+    importAsync(componentName)
+    {
+        return new Promise(resolve =>
+        {
+            const imported = this.import(componentName);
+            resolve(imported);
+        });
+    }
     import(componentName)
     {
         const resource = Resource.all[componentName];
@@ -1864,7 +1872,7 @@ try
             getValue: GM_getValue,
             setValue: GM_setValue,
             setClipboard: GM_setClipboard,
-            addValueChangeListener: GM_addValueChangeListener
+            addValueChangeListener: typeof GM_addValueChangeListener === "undefined" ? undefined : GM_addValueChangeListener,
         },
     });
     const applyScripts = () => resources.fetch()
