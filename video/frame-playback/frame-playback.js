afbe436a4a645f19456f0eeac0b42f55f87edac4
the1812 <the1812@outlook.com>
2019-03-07 16:08:51 +0800

    Fix bug in frame playback

--- video/frame-playback/frame-playback.js
+++ video/frame-playback/frame-playback.js
@@ -2,28 +2,36 @@ import html from 'framePlaybackHtml';
 resources.applyStyle("framePlaybackStyle");
 const main = async () =>
 {
+    const video = await SpinQuery.select(() => document.querySelector("video"));
+    const time = await SpinQuery.select(() => document.querySelector(".bilibili-player-video-time"));
+
     if (document.querySelector(".frame-playback"))
     {
         return;
     }
-    const video = await SpinQuery.select(() => document.querySelector("video"));
-    const time = await SpinQuery.select(() => document.querySelector(".bilibili-player-video-time"));
     time.insertAdjacentHTML("afterend", html);
-    const quality = parseInt(await SpinQuery.select(() => document.querySelector(".bilibili-player-video-quality-menu .bui-select-item-active")));
-    const fps = (() =>
+
+    await SpinQuery.select(() => document.querySelector(".bilibili-player-video-quality-menu .bui-select-item-active"));
+    let frameTime = 0;
+    Observer.attributesSubtree(".bilibili-player-video-quality-menu ul.bui-select-list", () =>
     {
-        switch (quality)
+        const quality = parseInt(document.querySelector(".bilibili-player-video-quality-menu .bui-select-item-active"));
+        const fps = (() =>
         {
-            // 60fps
-            case 116:
-            case 74:
-                return 60000 / 1001;
-            // 30fps
-            default:
-                return 30000 / 1001;
-        }
-    })();
-    const frameTime = 1 / fps;
+            switch (quality)
+            {
+                // 60fps
+                case 116:
+                case 74:
+                    return 60000 / 1001;
+                // 30fps
+                default:
+                    return 30000 / 1001;
+            }
+        })();
+        frameTime = 1 / fps;
+        console.log(frameTime);
+    });
     document.querySelector(".prev-frame").addEventListener("click", () => video.currentTime -= frameTime);
     document.querySelector(".next-frame").addEventListener("click", () => video.currentTime += frameTime);
 };
