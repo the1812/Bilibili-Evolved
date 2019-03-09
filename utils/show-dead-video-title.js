eed142733dbb46a546e7f95b90b029ee95d96c0a
the1812 <the1812@outlook.com>
2019-03-09 19:50:04 +0800

    Use page list API to get dead video title

--- utils/show-dead-video-title.js
+++ utils/show-dead-video-title.js
@@ -1,11 +1,26 @@
 function showTitle()
 {
     const deadVideos = $(".disabled[data-aid]").removeClass("disabled");
-    deadVideos.each((_, it) =>
+    deadVideos.each(async (_, it) =>
     {
         const $it = $(it);
         const aid = $it.attr("data-aid");
-        const title = $it.find("img").attr("alt");
+        let title = $it.find("img").attr("alt");
+        if (title === "已失效视频")
+        {
+            const response = JSON.parse(await Ajax.getText(`https://api.bilibili.com/x/player/pagelist?aid=${aid}`));
+            if (response.code === 0)
+            {
+                const titles = response.data.map(it => it.part);
+                [title] = titles;
+
+            }
+            else
+            {
+                console.warn("[显示失效视频信息] Page List API 未成功.");
+                console.warn(response);
+            }
+        }
         const link = (() =>
         {
             if (settings.useBiliplusRedirect)
@@ -17,8 +32,8 @@ function showTitle()
                 return `//www.bilibili.com/video/av${aid}`;
             }
         })();
-        $it.find(".i-watchlater")
-            .css("display", "none");
+        // $it.find(".i-watchlater")
+        //     .css("display", "none");
         $it.find("a.cover")
             .attr("target", "_blank")
             .attr("href", link);
