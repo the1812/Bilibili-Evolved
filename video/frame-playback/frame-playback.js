5433754f7576cff9afd32f228625ca91dd762b1b
the1812 <the1812@outlook.com>
2019-03-10 22:41:49 +0800

    Hide frame buttons in old player, add keyboard shortcut

--- video/frame-playback/frame-playback.js
+++ video/frame-playback/frame-playback.js
@@ -5,17 +5,19 @@ const main = async () =>
     const video = await SpinQuery.select(() => document.querySelector("video"));
     const time = await SpinQuery.select(() => document.querySelector(".bilibili-player-video-time"));
 
-    if (document.querySelector(".frame-playback"))
+    if (time === null || document.querySelector(".frame-playback"))
     {
         return;
     }
     time.insertAdjacentHTML("afterend", html);
 
-    await SpinQuery.select(() => document.querySelector(".bilibili-player-video-quality-menu .bui-select-item-active"));
     let frameTime = 0;
+    const prevFrame = () => video.currentTime -= frameTime;
+    const nextFrame = () => video.currentTime += frameTime;
     Observer.attributesSubtree(".bilibili-player-video-quality-menu ul.bui-select-list", () =>
     {
-        const quality = parseInt(document.querySelector(".bilibili-player-video-quality-menu .bui-select-item-active"));
+        const selectedQuality = document.querySelector(".bilibili-player-video-quality-menu .bui-select-item-active");
+        const quality = selectedQuality ? parseInt(selectedQuality.getAttribute("data-value")) : 0;
         const fps = (() =>
         {
             switch (quality)
@@ -30,9 +32,27 @@ const main = async () =>
             }
         })();
         frameTime = 1 / fps;
-        console.log(frameTime);
+        // console.log(frameTime);
     });
-    document.querySelector(".prev-frame").addEventListener("click", () => video.currentTime -= frameTime);
-    document.querySelector(".next-frame").addEventListener("click", () => video.currentTime += frameTime);
+    document.addEventListener("keydown", e =>
+    {
+        if (e.shiftKey)
+        {
+            if (e.key === "ArrowLeft")
+            {
+                e.stopPropagation();
+                e.preventDefault();
+                prevFrame();
+            }
+            else if (e.key === "ArrowRight")
+            {
+                e.stopPropagation();
+                e.preventDefault();
+                nextFrame();
+            }
+        }
+    });
+    document.querySelector(".prev-frame").addEventListener("click", prevFrame);
+    document.querySelector(".next-frame").addEventListener("click", nextFrame);
 };
 Observer.videoChange(main);
