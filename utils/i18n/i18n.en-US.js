c154a9c2d862ec1ef52dcee08e4d413196240347
the1812 <the1812@outlook.com>
2019-03-25 15:12:40 +0800

    Add some test translations

--- utils/i18n/i18n.en-US.js
+++ utils/i18n/i18n.en-US.js
@@ -1,5 +1,18 @@
 export const map = new Map([
-
+    [`主站`, `Home`],
+    [`画友`, `Paintings`],
+    [`音频`, `Music`],
+    [`游戏中心`, `Games`],
+    [`直播`, `Live`],
+    [`会员购`, `Shop`],
+    [`漫画`, `Comics`],
+    [`下载APP`, `APP`],
+    [`大会员`, `VIP`],
+    [`消息`, `Messages`],
+    [`动态`, `动态怎么翻啊`],
+    [`稍后再看`, `Watchlater`],
+    [`收藏`, `Favorites`],
+    [`历史`, `History`],
 ]);
 export default {
     export: { map },
