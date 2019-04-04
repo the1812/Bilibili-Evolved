2efeeddd83b5155aa1f7bca1f93f1c6f45d4c60d
the1812 <the1812@outlook.com>
2019-04-04 21:55:38 +0800

    Observer now can directly accept DOM Node

--- client/observer.js
+++ client/observer.js
@@ -24,7 +24,16 @@ export class Observer
     static observe(selector, callback, options)
     {
         callback([]);
-        return [...document.querySelectorAll(selector)].map(
+        let elements = selector;
+        if (typeof selector === "string")
+        {
+            elements = [...document.querySelectorAll(selector)];
+        }
+        else if (!Array.isArray(selector))
+        {
+            elements = [selector];
+        }
+        return elements.map(
             it =>
             {
                 const observer = new Observer(it, callback);
