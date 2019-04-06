d69066a85e8763bb7a835f168ee7745d6af059d6
the1812 <the1812@outlook.com>
2019-04-06 17:04:28 +0800

    Improve custom navbar

--- style/custom-navbar/custom-navbar.js
+++ style/custom-navbar/custom-navbar.js
@@ -18,7 +18,7 @@ class NavbarComponent
         this.popupHtml = ``;
         this.flex = `0 0 auto`;
         this.disabled = false;
-        this.showPopup = false;
+        this.href = ``;
     }
 }
 class Blank extends NavbarComponent
@@ -35,19 +35,31 @@ class Logo extends NavbarComponent
     constructor()
     {
         super();
-        this.html = /*html*/`<a href="https://www.bilibili.com/"><i class="custom-navbar-iconfont custom-navbar-icon-logo"></i></a>`;
+        this.href = `https://www.bilibili.com/`;
+        this.html = /*html*/`<i class="custom-navbar-iconfont custom-navbar-icon-logo"></i>`;
     }
 }
-class Home extends NavbarComponent
+class SimpleLink extends NavbarComponent
+{
+    constructor(name, link)
+    {
+        super();
+        this.html = name;
+        this.href = link;
+    }
+}
+class Category extends NavbarComponent
 {
     constructor()
     {
         super();
-        this.html = /*html*/`<a href="https://www.bilibili.com/">主站</a>`;
+        this.html = `主站`;
         this.popupHtml = /*html*/`
         <ul id="custom-navbar-home-popup">
             <li class="category-item" v-for="item of info">
-                {{item[0]}}<span>{{item[1]}}</span>
+                <a v-bind:href="item[1].link">
+                    {{item[0]}}<span>{{item[1].count}}</span>
+                </a>
             </li>
         </ul>
         `;
@@ -71,22 +83,27 @@ class Home extends NavbarComponent
         const regionCount = json.data.region_count;
         await SpinQuery.select("#custom-navbar-home-popup");
         return {
-            动画: regionCount[1],
-            番剧: regionCount[13],
-            国创: regionCount[167],
-            音乐: regionCount[3],
-            舞蹈: regionCount[129],
-            游戏: regionCount[4],
-            生活: regionCount[160],
-            鬼畜: regionCount[119],
-            时尚: regionCount[155],
-            广告: regionCount[165],
-            娱乐: regionCount[5],
-            影视: regionCount[181],
-            科技: regionCount[36],
-            数码: regionCount[188],
-            放映厅: regionCount[177] + regionCount[23],
+            动画: { count: regionCount[1], link: `https://www.bilibili.com/v/douga/` },
+            番剧: { count: regionCount[13], link: `https://www.bilibili.com/anime/` },
+            国创: { count: regionCount[167], link: `https://www.bilibili.com/guochuang/` },
+            音乐: { count: regionCount[3], link: `https://www.bilibili.com/v/music/` },
+            舞蹈: { count: regionCount[129], link: `https://www.bilibili.com/v/dance/` },
+            游戏: { count: regionCount[4], link: `https://www.bilibili.com/v/game/` },
+            科技: { count: regionCount[36], link: `https://www.bilibili.com/v/technology/` },
+            数码: { count: regionCount[188], link: `https://www.bilibili.com/v/digital/` },
+            生活: { count: regionCount[160], link: `https://www.bilibili.com/v/life/` },
+            鬼畜: { count: regionCount[119], link: `https://www.bilibili.com/v/kichiku/` },
+            时尚: { count: regionCount[155], link: `https://www.bilibili.com/v/fashion/` },
+            广告: { count: regionCount[165], link: `https://www.bilibili.com/v/ad/ad/` },
+            娱乐: { count: regionCount[5], link: `https://www.bilibili.com/v/ent/` },
+            影视: { count: regionCount[181], link: `https://www.bilibili.com/v/cinephile/` },
+            放映厅: { count: regionCount[177] + regionCount[23], link: `https://www.bilibili.com/cinema/` },
             // TODO: regionCount[11]?
+            专栏: { count: ``, link: `https://www.bilibili.com/read/home` },
+            直播: { count: ``, link: `https://live.bilibili.com` },
+            小黑屋: { count: ``, link: `https://www.bilibili.com/blackroom/` },
+            话题: { count: ``, link: `https://www.bilibili.com/blackboard/topic_list.html` },
+            活动: { count: ``, link: `https://www.bilibili.com/blackboard/x/act_list` },
         };
     }
 }
@@ -109,7 +126,11 @@ class Home extends NavbarComponent
     }
     const components = [
         new Logo,
-        new Home,
+        new Category,
+        new SimpleLink("画友", "https://h.bilibili.com"),
+        new SimpleLink("音频", "https://www.bilibili.com/audio/home/?type=10"),
+        new SimpleLink("会员购", "https://show.bilibili.com/platform/home.html?msource=pc_web"),
+        new SimpleLink("漫画", "https://manga.bilibili.com"),
         new Blank,
     ];
     new Vue({
@@ -117,11 +138,5 @@ class Home extends NavbarComponent
         data: {
             components,
         },
-        methods: {
-            togglePopup(component)
-            {
-                component.showPopup = !component.showPopup;
-            }
-        }
     });
 })();
