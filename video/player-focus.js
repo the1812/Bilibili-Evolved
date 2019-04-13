b4ce42620b546dd904f655197ea4e7100ca8e24f
the1812 <the1812@outlook.com>
2019-04-13 12:07:19 +0800

    Fix bugs

--- video/player-focus.js
+++ video/player-focus.js
@@ -1,7 +1,12 @@
 const target = document.URL.includes("bangumi") ? "#bofqi" : ".video-info .video-title .tit";
 SpinQuery.count(".nav-con,#bofqi", 3).then(() =>
 {
-    document.querySelector(target).scrollIntoView();
+    const element = document.querySelector(target);
+    if (element === null)
+    {
+        return;
+    }
+    element.scrollIntoView();
     if (settings.playerFocusOffset !== 0)
     {
         window.scrollBy(0, settings.playerFocusOffset);
