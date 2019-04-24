aae60e736c4958a7fb48f671429040abe90d723f
the1812 <the1812@outlook.com>
2019-04-24 12:07:05 +0800

    Acrtlic blur test

--- extras/vld-windows/VideoLinkDownloader/MainWindow.xaml.cs
+++ extras/vld-windows/VideoLinkDownloader/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +21,23 @@ namespace VideoLinkDownloader
         public MainWindow()
         {
             InitializeComponent();
+            button.Click += (s,e) =>
+            {
+                AcrylicBlur.Apply(this, new Color
+                {
+                    A = 0,
+                    R = 255,
+                    G = 255,
+                    B = 255,
+                }, AccentState.Diabled);
+                AcrylicBlur.Apply(this, new Color
+                {
+                    A = 0,
+                    R = 255,
+                    G = 255,
+                    B = 255,
+                }, AccentState.AcrylicBlurBehind);
+            };
         }
     }
 }
