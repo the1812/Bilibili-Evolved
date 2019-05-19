fe041400f9c8c58d5dbee6a07e3ac91810abb16b
the1812 <the1812@outlook.com>
2019-05-19 21:42:23 +0800

    Update description

--- builder/dotnet/Description.cs
+++ builder/dotnet/Description.cs
@@ -8,7 +8,7 @@ namespace BilibiliEvolved.Build
 {
     static class Description
     {
-        public static readonly string Master = "增强哔哩哔哩Web端体验: 下载视频, 音乐, 封面, 弹幕; 自定义播放器的画质, 模式, 布局; 删除广告, 使用夜间模式, 修复界面瑕疵; 以及增加对触屏设备的支持等.";
+        public static readonly string Master = "增强哔哩哔哩Web端体验: 下载视频, 音乐, 封面, 弹幕; 自定义播放器的画质, 模式, 布局; 自定义顶栏, 删除广告, 使用夜间模式; 以及增加对触屏设备的支持等.";
         public static readonly string Preview = "Bilibili Evolved 的预览版, 可以抢先体验新功能.";
         public static readonly string Offline = "Bilibili Evolved 的离线版, 所有功能都已内置于脚本中.";
         public static readonly string PreviewOffline = "Bilibili Evolved 的预览离线版, 可以抢先体验新功能, 并且所有功能都已内置于脚本中.";
