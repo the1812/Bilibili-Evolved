31f5978ef73a22beda227cbc5b79b96626b6445e
the1812 <the1812@outlook.com>
2019-05-22 22:43:23 +0800

    Fix bug in player shadow

--- style/player-shadow.js
+++ style/player-shadow.js
@@ -1,9 +1,11 @@
 const style = `<style id="player-shadow-style">
-    #bilibiliPlayer
+    #bilibiliPlayer,
+    #bofqi.mini-player::before
     {
         box-shadow: 0px 2px 8px 0px var(--theme-color-30) !important;
     }
-    body.dark #bilibiliPlayer
+    body.dark #bilibiliPlayer,
+    body.dark #bofqi.mini-player::before
     {
         box-shadow: 0px 2px 8px 0px var(--theme-color-20) !important;
     }
