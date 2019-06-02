ece0eab8a803c6c03f2ce3ada0970427af1e487c
the1812 <the1812@outlook.com>
2019-06-02 17:37:50 +0800

    Fix regex for Firefox

--- video/screenshot/screenshot.ts
+++ video/screenshot/screenshot.ts
@@ -39,7 +39,7 @@ class Screenshot
     }
     get filename()
     {
-        return `${getFriendlyTitle()} @${this.time.replace(/:/g, "-")} ${this.timeStamp.toString()}.png`;
+        return `${getFriendlyTitle()} @${this.time.replace(/\:/g, "-")} ${this.timeStamp.toString()}.png`;
     }
     get id()
     {
