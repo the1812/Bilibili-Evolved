2009d139958feb2632187a8510991b6bb4d058c0
the1812 <the1812@outlook.com>
2019-06-06 00:23:10 +0800

    Add i18n-sync script

--- builder/node/i18n-sync/i18n-sync.js
+++ builder/node/i18n-sync/i18n-sync.js
@@ -28,11 +28,18 @@ const extractKeys = (language) => {
     return new LanguageKeys(mapKeys, regexKeys);
 };
 const mainLanguageKey = extractKeys(readFile(path_1.join(config.folder, `i18n.${config.mainLanguage}.js`)));
-const targetLanguages = config.targetLanguages.map(l => readFile(path_1.join(config.folder, `i18n.${l}.js`)));
+const targetLanguages = config.targetLanguages.map(name => {
+    const path = path_1.join(config.folder, `i18n.${name}.js`);
+    return {
+        name,
+        path,
+        text: readFile(path)
+    };
+});
 targetLanguages.forEach(language => {
-    const diff = mainLanguageKey.diff(extractKeys(language));
-    const output = language
+    const diff = mainLanguageKey.diff(extractKeys(language.text));
+    const output = language.text
         .replace(/([\s\n]*\[`\*`)/, `\n  ${diff.mapKeys.map(k => '[`' + k + '`, `TODO:`],').join('\n  ')}$1`)
         .replace(/(\]\);?[\s\n]*export default)/, `  ${diff.regexKeys.map(k => '[' + k + ', `TODO:`],').join('\n  ')}\n$1`);
-    console.log(output);
+    fs_1.writeFileSync(language.path, output, { encoding: 'utf-8' });
 });
--- builder/node/i18n-sync/i18n-sync.ts
+++ builder/node/i18n-sync/i18n-sync.ts
@@ -1,4 +1,4 @@
-import { readFileSync } from 'fs';
+import { readFileSync, writeFileSync } from 'fs';
 import { join } from 'path';
 
 interface Config {
@@ -38,11 +38,18 @@ const extractKeys = (language: string) => {
 }
 
 const mainLanguageKey = extractKeys(readFile(join(config.folder, `i18n.${config.mainLanguage}.js`)))
-const targetLanguages = config.targetLanguages.map(l => readFile(join(config.folder, `i18n.${l}.js`)))
+const targetLanguages = config.targetLanguages.map(name => {
+  const path = join(config.folder, `i18n.${name}.js`)
+  return {
+    name,
+    path,
+    text: readFile(path)
+  }
+})
 targetLanguages.forEach(language => {
-  const diff = mainLanguageKey.diff(extractKeys(language))
-  const output = language
+  const diff = mainLanguageKey.diff(extractKeys(language.text))
+  const output = language.text
     .replace(/([\s\n]*\[`\*`)/, `\n  ${diff.mapKeys.map(k => '[`' + k + '`, `TODO:`],').join('\n  ')}$1`)
     .replace(/(\]\);?[\s\n]*export default)/, `  ${diff.regexKeys.map(k => '[' + k + ', `TODO:`],').join('\n  ')}\n$1`)
-  console.log(output)
+  writeFileSync(language.path, output, { encoding: 'utf-8' })
 })
