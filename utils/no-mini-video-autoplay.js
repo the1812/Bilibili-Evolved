c2ba83d03f463e924a782b783e4e299b072bed57
the1812 <the1812@outlook.com>
2019-06-14 11:40:07 +0800

    Hide mini video option

---
+++ utils/no-mini-video-autoplay.js
@@ -0,0 +1,8 @@
+// if (document.URL.replace(window.location.search, '') === 'https://t.bilibili.com/') {
+//   Observer.attributes('.vc-video>.bilibili-link-player', records => {
+//     records.forEach(record => {
+//       if (record.attributeName === 'data-playing-state') {
+//       }
+//     })
+//   })
+// }
---
+++ utils/no-mini-video-autoplay.ts
@@ -0,0 +1,9 @@
+// if (document.URL.replace(window.location.search, '') === 'https://t.bilibili.com/') {
+//   Observer.attributes('.vc-video>.bilibili-link-player', records => {
+//     records.forEach(record => {
+//       if (record.attributeName === 'data-playing-state') {
+
+//       }
+//     })
+//   })
+// }
