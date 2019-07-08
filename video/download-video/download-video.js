3f1822df3f2952c0245c054de3219d5404b749bd
the1812 <the1812@outlook.com>
2019-07-08 13:53:54 +0800

    Add dark slice 9

--- video/download-video/download-video.js
+++ video/download-video/download-video.js
@@ -252,6 +252,7 @@ ${it.url}
   referer=${document.URL.replace(window.location.search, '')}
   user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:67.0) Gecko/20100101 Firefox/67.0
   out=${getFriendlyTitle(true)}${indexNumber}.flv
+  split=${this.fragmentSplitFactor}
   `.trim()
 }).join('\n')}
       `.trim()
