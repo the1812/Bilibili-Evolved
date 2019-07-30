ea771a8cb9dd47645105edc3224eef11bc0d92f7
Coulomb_G <965145921@qq.com>
2019-07-30 16:05:02 +0800

    日本語翻訳の更新

--- bilibili-evolved.offline.user.js
+++ bilibili-evolved.offline.user.js
@@ -1,10 +1,6 @@
 // ==UserScript==
 // @name         Bilibili Evolved (Offline)
-<<<<<<< HEAD
-// @version      378.94
-=======
-// @version      378.95
->>>>>>> 07f87ef3243df52dbbe793c007120007341d1a21
+// @version      379.01
 // @description  Bilibili Evolved 的离线版, 所有功能都已内置于脚本中.
 // @author       Grant Howard, Coulomb-G
 // @copyright    2019, Grant Howard (https://github.com/the1812) & Coulomb-G (https://github.com/Coulomb-G)
--- bilibili-evolved.preview-offline.user.js
+++ bilibili-evolved.preview-offline.user.js
@@ -1,10 +1,6 @@
 // ==UserScript==
 // @name         Bilibili Evolved (Preview Offline)
-<<<<<<< HEAD
-// @version      378.94
-=======
-// @version      378.95
->>>>>>> 07f87ef3243df52dbbe793c007120007341d1a21
+// @version      379.01
 // @description  Bilibili Evolved 的预览离线版, 可以抢先体验新功能, 并且所有功能都已内置于脚本中.
 // @author       Grant Howard, Coulomb-G
 // @copyright    2019, Grant Howard (https://github.com/the1812) & Coulomb-G (https://github.com/Coulomb-G)
