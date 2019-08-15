cc338c9934ee9bcc51555768a5fd1973c025d894
the1812 <the1812@outlook.com>
2019-08-15 11:38:39 +0800

    Fix bug in getFriendlyTitle

--- src/video/title.js
+++ src/video/title.js
@@ -24,6 +24,7 @@ export function formatTitle (format, includesPageTitle = true) {
     title: document.title
       .replace(includesPageTitle ? /：([^：]+?)_番剧_bilibili_哔哩哔哩/ : '_番剧_bilibili_哔哩哔哩', '')
       .replace(includesPageTitle ? /：([^：]+?)_电影_bilibili_哔哩哔哩/ : '_电影_bilibili_哔哩哔哩', '')
+      .replace(includesPageTitle ? /：([^：]+?)_纪录片_bilibili_哔哩哔哩/ : '_纪录片_bilibili_哔哩哔哩', '')
       .replace('_哔哩哔哩 (゜-゜)つロ 干杯~-bilibili', '')
       .replace(/(.*?) - (.*?) - 哔哩哔哩直播，二次元弹幕直播平台/, '$1')
       .trim(),
