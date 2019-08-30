208e5e8f376ba59a1eb8060d0b105a60d1c91f69
the1812 <the1812@outlook.com>
2019-08-30 15:05:05 +0800

    Update version number

--- src/client/bilibili-evolved.js
+++ src/client/bilibili-evolved.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Bilibili Evolved (Preview)
-// @version      1.8.18
+// @version      1.8.19
 // @description  Bilibili Evolved 的预览版, 可以抢先体验新功能.
 // @author       Grant Howard, Coulomb-G
 // @copyright    2019, Grant Howard (https://github.com/the1812) & Coulomb-G (https://github.com/Coulomb-G)
