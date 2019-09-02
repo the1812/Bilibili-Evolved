5776f834a84438c65d4b5641777cb2319b27566a
the1812 <the1812@outlook.com>
2019-09-02 22:32:01 +0800

    Add bundle builder, remove vld-windows

--- builder/dotnet/BundleBuilder.cs
+++ builder/dotnet/BundleBuilder.cs
@@ -4,7 +4,10 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Security.Cryptography;
 using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.IO.Compression;
 
 namespace BilibiliEvolved.Build
 {
@@ -12,14 +15,23 @@ namespace BilibiliEvolved.Build
   {
     public ProjectBuilder BuildBundle()
     {
-      // var urlList = from file in Directory.GetFiles("min")
-      //               where !file.Contains("dark-slice")
-      //               select file.Replace(@"\", "/");
-      // var hashDict = new Dictionary<string, string>();
-      // foreach (var url in urlList)
-      // {
-
-      // }
+      var urlList = from file in Directory.GetFiles("min")
+                    where !file.Contains("dark-slice")
+                    select file.Replace(@"\", "/");
+      var hashDict = new Dictionary<string, string>();
+      using (var sha1 = new SHA1Managed())
+      using (var zip = ZipFile.Open("min/bundle.zip", ZipArchiveMode.Update))
+      {
+        foreach (var url in urlList)
+        {
+          var filename = Path.GetFileName(url);
+          var hash = string.Join("", sha1.ComputeHash(File.OpenRead(url)).Select(b => b.ToString("X2")).ToArray());
+          zip.CreateEntryFromFile(url, filename);
+          hashDict.Add(filename, hash);
+        }
+      }
+      File.WriteAllText("min/bundle.json", JsonConvert.SerializeObject(hashDict, Formatting.Indented));
+      WriteSuccess("Bundle build complete.");
       return this;
     }
   }
