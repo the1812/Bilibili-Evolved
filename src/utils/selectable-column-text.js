c622bf3f37e5bd8670cd5cbda23274aa77f2ed9a
the1812 <the1812@outlook.com>
2019-09-29 23:02:29 +0800

    Fix column text cannot copy

--- src/utils/selectable-column-text.js
+++ src/utils/selectable-column-text.js
@@ -1,2 +1,16 @@
-const style = `.article-holder p { user-select: text }`
-export default resources.toggleStyle(style, 'selectable-column-text-style')
+const style = `.article-holder { user-select: text !important }`
+const id = 'selectable-column-text-style'
+const load = () => {
+  resources.applyStyleFromText(style, id)
+  SpinQuery.unsafeJquery().then(async () => {
+    await SpinQuery.select('.article-holder')
+    unsafeWindow.$('.article-holder').unbind('copy')
+  })
+}
+load()
+export default {
+  reload: load,
+  unload: () => {
+    document.getElementById(id).remove()
+  },
+}
