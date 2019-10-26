71a5ca703b52c8a17f51669e984492773d431855
the1812 <the1812@outlook.com>
2019-10-26 11:01:14 +0800

    Add lazy loading for dpi-img

--- src/style/dpi-img.vue
+++ src/style/dpi-img.vue
@@ -1,17 +1,44 @@
 <template>
-  <img :width="width" :height="height" :srcset="srcset" :src="src" :style="{filter: blur ? 'blur(' + blur + 'px)' : undefined}">
+  <img :width="width" :height="height" :srcset="srcset" :src="actualSrc" :style="{filter: blur ? 'blur(' + blur + 'px)' : undefined}">
 </template>
 
 <script>
 export default {
-  props: ['size', 'src', 'blur'],
-  computed: {
-    srcset () {
+  props: ['size', 'src', 'blur', 'root', 'rootMargin', 'threshold'],
+  data() {
+    return {
+      srcset: null,
+      actualSrc: null,
+    }
+  },
+  methods: {
+    calcSrc() {
       if (!this.src || !this.size) {
         return null
       }
-      return getDpiSourceSet(this.src, this.size)
+      const extension = this.src.substring(this.src.lastIndexOf('.') + 1)
+      this.srcset = getDpiSourceSet(this.src, this.size, extension)
+      this.actualSrc = this.src
     },
+  },
+  mounted() {
+    const options = {
+      root: this.root,
+      rootMargin: this.rootMargin || '200px',
+      threshold: this.threshold,
+    }
+    console.log(options)
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          this.calcSrc()
+          observer.disconnect()
+        }
+      })
+    }, options)
+    observer.observe(this.$el)
+  },
+  computed: {
     width () {
       if (typeof this.size === 'object' && 'width' in this.size) {
         return this.size.width
