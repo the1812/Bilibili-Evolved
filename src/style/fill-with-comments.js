0f23184ddbf219450b7d8f751280250099019f78
the1812 <the1812@outlook.com>
2019-11-08 16:44:00 +0800

    Add fill with comments

---
+++ src/style/fill-with-comments.js
@@ -0,0 +1,21 @@
+const styleID = 'fill-with-comments-style'
+export const fillWithComments = (enable) => {
+  if (enable) {
+    resources.applyStyleFromText(`
+    .v-wrap { position: relative !important; }
+    .v-wrap .l-con { width: 100% !important; }
+    .v-wrap .r-con {
+      position: absolute !important;
+      top: 0 !important;
+      right: 0 !important;
+    }
+    `, styleID)
+  } else {
+    dqa('#' + styleID).forEach(it => it.remove())
+  }
+}
+export default {
+  export: {
+    fillWithComments,
+  },
+}
--- src/style/hide-related-videos.js
+++ src/style/hide-related-videos.js
@@ -1,3 +1,7 @@
+addSettingsListener('hideRecommendLive', async value => {
+  const { fillWithComments } = await import('./fill-with-comments')
+  fillWithComments(value)
+}, true)
 export default resources.toggleStyle(`
   #recom_module, #reco_list { display: none !important; }
   `, `hide-related-videos-style`);
