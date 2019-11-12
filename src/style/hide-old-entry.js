3202c0f49b1100d9a58eff07636374ffc3cb6289
the1812 <the1812@outlook.com>
2019-11-12 20:58:05 +0800

    Update old entry class

--- src/style/hide-old-entry.js
+++ src/style/hide-old-entry.js
@@ -1,16 +1,3 @@
-const style = `#entryOld, .main-container .entry-old { display: none !important; }`;
-const id = "hide-old-entry-style";
-const reload = () => resources.applyStyleFromText(/*html*/`<style id="${id}">${style}</style>`);
-const unload = () =>
-{
-    const styleElement = document.getElementById(id);
-    if (styleElement)
-    {
-        styleElement.remove();
-    }
-}
-reload();
-export default {
-    reload,
-    unload,
-};
+const style = `#entryOld, .main-container .entry-old, .try-feedback { display: none !important; }`
+const id = 'hide-old-entry-style'
+export default resources.toggleStyle(style, id)
