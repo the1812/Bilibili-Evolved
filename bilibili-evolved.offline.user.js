6276f342f70778a06dd1921020f0eb8472cff65e
Coulomb_G <965145921@qq.com>
2019-12-13 12:53:53 +0800

    https://t.bilibili.com/

--- bilibili-evolved.offline.user.js
+++ bilibili-evolved.offline.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Bilibili Evolved (Offline)
-// @version      514.12
+// @version      514.88
 // @description  Bilibili Evolved 的离线版, 所有功能都已内置于脚本中.
 // @author       Grant Howard, Coulomb-G
 // @copyright    2019, Grant Howard (https://github.com/the1812) & Coulomb-G (https://github.com/Coulomb-G)
