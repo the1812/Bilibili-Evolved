6fa580d6c92fc83cd4671e4dceb9fdeff8c6210e
the1812 <the1812@outlook.com>
2020-01-12 20:16:51 +0800

    Add ass subtitle support

--- src/video/download-subtitle/download-subtitle.ts
+++ src/video/download-subtitle/download-subtitle.ts
@@ -47,16 +47,57 @@ export default {
           }
           const language = (settingsPanel.querySelector('.bilibili-player-video-subtitle-setting-lan .bui-select-result') as HTMLElement).innerHTML
           const subtitle = subtitles.find(s => s.language === language) || subtitles[0]
-          // if (!subtitle) {
-          //   logError(`未找到对应<span>${language}</span>的字幕.`)
-          //   return
-          // }
           const json = await Ajax.getJson(subtitle.url)
           const rawData = json.body
+          const title = getFriendlyTitle(true)
           if (e.shiftKey) {
-            // TODO:
+            const { SubtitleConverter, SubtitleSize, SubtitleLocation } = await import('./subtitle-converter')
+
+            const fontSizeThumb = settingsPanel.querySelector('.bilibili-player-video-subtitle-setting-fontsize .bui-thumb') as HTMLElement
+            const translateX = parseFloat(fontSizeThumb.style.transform!.replace(/translateX\(([\d\.]+)/, '$1'))
+            const fontSizeMapping: { [key: number]: number } = {
+              214: SubtitleSize.VeryLarge,
+              '163.5': SubtitleSize.Large,
+              107: SubtitleSize.Medium,
+              '50.5': SubtitleSize.Small,
+              0: SubtitleSize.VerySmall,
+            }
+            const size = fontSizeMapping[translateX]
+
+            const colorSpan = settingsPanel.querySelector('.bilibili-player-video-subtitle-setting-color .bui-select-result span:first-child') as HTMLElement
+            const color = colorSpan.getAttribute('style')!.match(/background:[ ]*(.+);/)![1]
+
+            const opacityDiv = settingsPanel.querySelector('.bilibili-player-video-subtitle-setting-opacity .bui-bar') as HTMLElement
+            const opacity = parseFloat(opacityDiv.style.transform!.replace(/scaleX\(([\d\.]+)/, '$1'))
+
+            const positionDiv = dq('.subtitle-position') as HTMLElement
+            const positions = {
+              bc: SubtitleLocation.BottomCenter,
+              bl: SubtitleLocation.BottomLeft,
+              br: SubtitleLocation.BottomRight,
+              tc: SubtitleLocation.TopCenter,
+              tl: SubtitleLocation.TopLeft,
+              tr: SubtitleLocation.TopRight,
+            }
+            const location = Object.entries(positions).filter(([name]) => {
+              return positionDiv.classList.contains(`subtitle-position-${name}`)
+            }).map(([, location]) => location).shift()!
+
+            const video = dq('video') as HTMLVideoElement
+            const config = {
+              title,
+              height: video.videoHeight,
+              width: video.videoWidth,
+              color,
+              location,
+              opacity,
+              size,
+            }
+            const converter = new SubtitleConverter(config)
+            const ass = await converter.convertToAss(rawData)
+            download(ass, title + '.ass')
           } else {
-            download(JSON.stringify(rawData), getFriendlyTitle(true) + '.json')
+            download(JSON.stringify(rawData), title + '.json')
           }
         } catch (error) {
           logError(error)
