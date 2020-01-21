6436ac8d9533538f2b814f50ebf61e09510d3f20
the1812 <the1812@outlook.com>
2020-01-21 17:36:00 +0800

    Fix player focus condition

--- src/video/player-focus.js
+++ src/video/player-focus.js
@@ -1,14 +1,25 @@
-const target = document.URL.includes("bangumi") ? "#bofqi" : ".video-info .video-title .tit";
-SpinQuery.count(".nav-con,#bofqi", 3).then(() =>
-{
-    const element = document.querySelector(target);
-    if (element === null)
-    {
-        return;
-    }
-    element.scrollIntoView();
-    if (settings.playerFocusOffset !== 0)
-    {
-        window.scrollBy(0, settings.playerFocusOffset);
-    }
-});
+const target = document.URL.includes('bangumi') ? '#bofqi' : '.video-info .video-title .tit'
+// SpinQuery.count('.nav-con,#bofqi', 3).then(() => {
+//   const element = document.querySelector(target)
+//   if (element === null) {
+//     return
+//   }
+//   element.scrollIntoView()
+//   if (settings.playerFocusOffset !== 0) {
+//     window.scrollBy(0, settings.playerFocusOffset)
+//   }
+// })
+SpinQuery.select(target).then(async element => {
+  await SpinQuery.condition(() => dq('.video-toolbar .ops .collect,.tool-bar .coin-info'), it => {
+    return it !== null && it.innerText !== '--'
+  })
+  console.log(element)
+  if (element === null) {
+    return
+  }
+  element.scrollIntoView()
+  if (settings.playerFocusOffset !== 0) {
+    window.scrollBy(0, settings.playerFocusOffset)
+  }
+  console.log(element.offsetTop, settings.playerFocusOffset, window.scrollY)
+})
