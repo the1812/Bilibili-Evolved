169ec6b81b16c18b10422357c6827bf8e8584b15
the1812 <the1812@outlook.com>
2020-01-21 17:44:59 +0800

    Fix not work after video change

--- src/video/skip-charge-list/skip-charge-list.js
+++ src/video/skip-charge-list/skip-charge-list.js
@@ -1,15 +1,8 @@
-async function skipChargeList()
-{
-    const video = await SpinQuery.select(() => document.querySelector("video"));
-    video && video.addEventListener("ended", async () =>
-    {
-        const jumpButton = await SpinQuery.select(() => document.querySelector(".bilibili-player-electric-panel-jump"));
-        jumpButton && jumpButton.click();
-    });
+async function skipChargeList () {
+  const video = await SpinQuery.select(() => document.querySelector('video'))
+  video && video.addEventListener('ended', async () => {
+    const jumpButton = await SpinQuery.select(() => document.querySelector('.bilibili-player-electric-panel-jump'))
+    jumpButton && jumpButton.click()
+  })
 }
-if (Observer.videoChange)
-{
-    Observer.videoChange(skipChargeList);
-}
-else
-{ Observer.childList("#bofqi", skipChargeList); }
+Observer.videoChange(skipChargeList)
