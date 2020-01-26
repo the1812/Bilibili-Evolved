ad53c75572bcd91091f57aae966bc77e9499504e
the1812 <the1812@outlook.com>
2020-01-26 11:01:00 +0800

    Remove duplicated collaboration videos in feeds

--- src/style/custom-navbar/activities/custom-navbar-activities.ts
+++ src/style/custom-navbar/activities/custom-navbar-activities.ts
@@ -220,7 +220,10 @@ export class Activities extends NavbarComponent {
           handleJson: async function (json) {
             // const { getWatchlaterList } = await import('../../video/watchlater-api')
             // const watchlaterList = await getWatchlaterList()
-            const cards = json.data.cards.map((card: any) => {
+            const cards = json.data.cards.filter((c: any) => {
+              // 合作视频仅取UP主的
+              return c.desc.orig_dy_id === 0
+            }).map((card: any) => {
               const cardJson = JSON.parse(card.card)
               return {
                 coverUrl: cardJson.pic,
