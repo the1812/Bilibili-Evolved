28be16df2caf1a30de6fe70612cce548a37bb98e
the1812 <the1812@outlook.com>
2020-04-06 10:35:10 +0800

    Detect URL

--- src/utils/selectable-column-text.js
+++ src/utils/selectable-column-text.js
@@ -1,6 +1,9 @@
 const style = `.article-holder { user-select: text !important }`
 const id = 'selectable-column-text-style'
 const load = () => {
+  if (!document.URL.startsWith('https://www.bilibili.com/read/')) {
+    return
+  }
   resources.applyStyleFromText(style, id)
   SpinQuery.unsafeJquery().then(async () => {
     if (!unsafeWindow.$) {
