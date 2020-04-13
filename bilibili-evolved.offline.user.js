3284a9b210485f51b1b8992fc3054bc39ce2bd3b
the1812 <the1812@outlook.com>
2020-04-13 16:57:31 +0800

    Update version number

--- bilibili-evolved.offline.user.js
+++ bilibili-evolved.offline.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Bilibili Evolved (Offline)
-// @version      637.02
+// @version      637.05
 // @description  Bilibili Evolved 的离线版, 所有功能都已内置于脚本中.
 // @author       Grant Howard, Coulomb-G
 // @copyright    2020, Grant Howard (https://github.com/the1812) & Coulomb-G (https://github.com/Coulomb-G)
