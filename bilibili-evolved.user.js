b770a27a1f1d53d082b53a1a9c190cb61dfa0dc0
the1812 <the1812@outlook.com>
2020-05-10 10:39:38 +0800

    Update version number

--- bilibili-evolved.user.js
+++ bilibili-evolved.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Bilibili Evolved
-// @version      1.10.20
+// @version      1.10.21
 // @description  强大的哔哩哔哩增强脚本: 下载视频, 音乐, 封面, 弹幕 / 简化直播间, 评论区, 首页 / 自定义顶栏, 删除广告, 夜间模式 / 触屏设备支持
 // @author       Grant Howard, Coulomb-G
 // @copyright    2020, Grant Howard (https://github.com/the1812) & Coulomb-G (https://github.com/Coulomb-G)
