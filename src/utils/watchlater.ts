278baddbcf8c25f1c4feffda4800b06c2781870d
the1812 <the1812@outlook.com>
2020-05-15 10:33:08 +0800

    Convert watchlater redirect to ts

--- src/utils/watchlater.ts
+++ src/utils/watchlater.ts
@@ -1,4 +1,4 @@
-const getRedirectLink = text => {
+const getRedirectLink = (text: string) => {
   const match = text.match(/(av[\d]+)\/p([\d]+)/i) || text.match(/(BV[\w]+)\/p([\d]+)/i)
   if (match) {
     return `https://www.bilibili.com/video/${match[1]}/?p=${match[2]}`
@@ -6,7 +6,7 @@ const getRedirectLink = text => {
     return 'javascript:;'
   }
 }
-const redirectLinks = items => {
+const redirectLinks = (items: HTMLElement[]) => {
   const watchlaterList = items
     .map(it => {
       const href = it.getAttribute('href')
@@ -19,17 +19,18 @@ const redirectLinks = items => {
       if (href.match(/video\/av|video\/BV/i)) {
         return href
       }
+      return href
     })
   items.forEach((it, index) => {
     it.setAttribute('href', watchlaterList[index])
     it.setAttribute('target', '_blank')
   })
 }
-const redirectSelectors = (...selectors) => {
+const redirectSelectors = (...selectors: string[]) => {
   for (const selector of selectors) {
     SpinQuery.select(
       () => document.querySelectorAll(selector),
-      it => redirectLinks([...it])
+      it => redirectLinks([...it] as HTMLElement[])
     )
   }
 }
@@ -37,7 +38,7 @@ SpinQuery.select('.watch-later-list').then(() => {
   Observer.childListSubtree('#viewlater-app', () => {
     SpinQuery.condition(
       () => document.URL.match(/(av[\d]+)\/p([\d]+)/i) || document.URL.match(/(BV[\w]+)\/p([\d]+)/i),
-      it => it && document.URL.indexOf('watchlater') !== -1,
+      it => it !== null && document.URL.indexOf('watchlater') !== -1,
       () => {
         const url = getRedirectLink(document.URL)
         if (url !== null) {
@@ -68,14 +69,17 @@ SpinQuery.select('.van-popper-favorite').then(async favoritePopup => {
     if (playAll) {
       const url = '//www.bilibili.com/watchlater/#/list'
       Observer.attributes(playAll, () => {
+        if (playAll.firstElementChild === null || playAll.lastChild === null) {
+          return
+        }
         if (playAll.getAttribute('href') === '//www.bilibili.com/watchlater/') {
           playAll.setAttribute('href', url)
-          playAll.firstChild.classList.remove('bili-icon_dingdao_bofang')
-          playAll.firstChild.classList.add('bili-icon_xinxi_yuedushu')
+          playAll.firstElementChild.classList.remove('bili-icon_dingdao_bofang')
+          playAll.firstElementChild.classList.add('bili-icon_xinxi_yuedushu')
           playAll.lastChild.nodeValue = '查看全部'
         } else if (playAll.getAttribute('href') !== url) {
-          playAll.firstChild.classList.add('bili-icon_dingdao_bofang')
-          playAll.firstChild.classList.remove('bili-icon_xinxi_yuedushu')
+          playAll.firstElementChild.classList.add('bili-icon_dingdao_bofang')
+          playAll.firstElementChild.classList.remove('bili-icon_xinxi_yuedushu')
           playAll.lastChild.nodeValue = '播放全部'
         }
       })
