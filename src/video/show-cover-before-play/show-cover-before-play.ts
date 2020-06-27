5807a58e35c967874f47bdf6cd9cf498ac26e8ed
the1812 <the1812@outlook.com>
2020-06-27 11:49:18 +0800

    Fix bugs in bangumi page

--- src/video/show-cover-before-play/show-cover-before-play.ts
+++ src/video/show-cover-before-play/show-cover-before-play.ts
@@ -5,40 +5,50 @@ const url = {
   ]
 }
 const styleID = 'showCoverBeforePlayStyle'
-let lastAid: string
+let lastCid: string
 const entry = () => {
   const showCover = async () => {
     if (url.include.every(it => !document.URL.includes(it))) {
       return
     }
+    const removeCover = () => document.body.style.removeProperty('--cover-url')
     resources.applyStyle(styleID)
     const aid = await SpinQuery.select(() => unsafeWindow.aid)
     if (!aid) {
       console.warn('[播放前显示封面] 未找到av号')
+      removeCover()
       return
     }
-    if (aid === lastAid) {
+    const { cid } = unsafeWindow
+    if (cid === lastCid || !cid) {
+      removeCover()
       return
     }
-    lastAid = aid
+    lastCid = cid
     const video = await SpinQuery.select('video') as HTMLVideoElement
     if (!video) {
       console.warn('[播放前显示封面] 未找到视频')
-      return
-    }
-    if (!video.paused) {
+      removeCover()
       return
     }
     const { VideoInfo } = await import('../video-info')
     const info = new VideoInfo(aid)
     await info.fetchInfo()
+    if (!video.paused) {
+      removeCover()
+      return
+    }
     document.body.style.setProperty('--cover-url', `url('${info.coverUrl}')`)
     video.addEventListener('play', () => {
-      document.body.style.removeProperty('--cover-url')
+      removeCover()
     }, { once: true })
   }
   // Observer.videoChange(showCover)
-  showCover()
+  if (document.URL.includes('//www.bilibili.com/bangumi/')) {
+    Observer.videoChange(showCover)
+  } else {
+    showCover()
+  }
 }
 entry()
 export default {
