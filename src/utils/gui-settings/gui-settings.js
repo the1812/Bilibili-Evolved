f5f4ddc87d26389e5aa31ff0e295d6320d85a572
the1812 <the1812@outlook.com>
2020-10-28 20:48:24 +0800

    Add line break

--- src/utils/gui-settings/gui-settings.js
+++ src/utils/gui-settings/gui-settings.js
@@ -144,6 +144,7 @@ const preCheckCompatibility = () => {
     Toast.info(/* html */`
 <div>为避免b站播放器改版导致网站无法正常使用, 以下功能已自动关闭并禁用:
 <span>自动定位到播放器</span> <span>外置稍后再看</span> <span>启用快速收藏</span>
+
 详情见<a target="_blank" href="https://github.com/the1812/Bilibili-Evolved/issues/1076" class="link">讨论区</a>, 这些功能将在恢复后再解除禁用.
 若当前页面是视频页面且出现问题, 刷新即可恢复正常.</div>
     `.trim(), '通知')
