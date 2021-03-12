ebf0cb17722e3581f96704fd52a9f6b55d87edcb
the1812 <the1812@outlook.com>
2021-03-12 14:34:24 +0800

    Fix subtitle expand

--- src/video/expand-description/expand-description.ts
+++ src/video/expand-description/expand-description.ts
@@ -6,10 +6,10 @@ const load = () => {
     if (!desc) {
       return
     }
-    const subtitle = dq(desc, '.subtitle') as HTMLElement
-    if (!subtitle) {
-      return
-    }
+    // const subtitle = dq(desc, '.subtitle') as HTMLElement
+    // if (!subtitle) {
+    //   return
+    // }
     const expandButton = await SpinQuery.select('.video-desc .btn[report-id="abstract_spread"]') as HTMLElement
     expandButton?.click()
   })
