077d0c3aaae3d9a5aef73fce0b22f74d8918f353
the1812 <the1812@outlook.com>
2021-04-24 17:35:38 +0800

    Improve comment-lite styles

--- src/style/comment/comment.ts
+++ src/style/comment/comment.ts
@@ -1,1 +1,4 @@
+addSettingsListener('useCommentStyle', (value: boolean) => {
+  document.body.classList.toggle('simplify-comment', value)
+}, true)
 export default resources.toggleStyle('useCommentStyleStyle')
