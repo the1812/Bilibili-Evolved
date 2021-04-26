6801c36868ae20c80d7f3f9b6b616aac1825fe51
the1812 <the1812@outlook.com>
2021-04-26 18:12:30 +0800

    Add logs

--- src/style/dark/dark-color-scheme.ts
+++ src/style/dark/dark-color-scheme.ts
@@ -1,11 +1,13 @@
 const matchList = matchMedia('(prefers-color-scheme: dark)')
 const check = async (isSystemDark: boolean) => {
   if (isSystemDark !== settings.useDarkStyle) {
+    console.log('[dark-color-scheme]', isSystemDark, settings.useDarkStyle)
     settings.useDarkStyle = isSystemDark
     if (isSystemDark) {
       await resources.fetchByKey('useDarkStyle')
     } else {
       const darkStyles = await import('./dark-styles')
+      console.log('[dark-color-scheme]', darkStyles)
       darkStyles?.unload()
     }
   }
