0459af061a4604050deaceec13473c6dc887acfa
the1812 <the1812@outlook.com>
2021-05-06 13:46:35 +0800

    Update styles (fix #1794)

--- src/style/hide-related-videos.ts
+++ src/style/hide-related-videos.ts
@@ -1,5 +1,5 @@
 export default resources.toggleStyle(`
-  #recom_module,#reco_list,.bilibili-player-ending-panel-box-videos {
+  #recom_module,#reco_list,.bilibili-player-ending-panel-box-videos,.r-con .rcmd-list {
     display: none !important;
   }
   .bilibili-player-ending-panel-box-functions .bilibili-player-upinfo-spans {
