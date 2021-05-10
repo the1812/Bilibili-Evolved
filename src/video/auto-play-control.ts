799b0fcaaa61d6abd7b66235afdf499ff1255f62
the1812 <the1812@outlook.com>
2021-05-10 14:47:43 +0800

    Improve multiple pages handling

--- src/video/auto-play-control.ts
+++ src/video/auto-play-control.ts
@@ -11,21 +11,32 @@
     enable: ['.multi-page .next-button', '.player-auxiliary-autoplay-switch input'],
     disable: ['.recommend-list .next-button'],
   }
+  const disableConditions = [
+    // 最后 1P 时不能开启连播
+    () => Boolean(dq('.multi-page .list-box li.on:last-child')),
+  ]
   const isChecked = (container: HTMLElement) => {
     return Boolean(container.querySelector('.switch-button.on, :checked'))
   }
   const { playerReady } = await import('./player-ready')
   await playerReady()
-  const element = await SpinQuery.select(
-    [...autoPlayControls.enable, ...autoPlayControls.disable].join(',')
-  )
-  if (!element) {
-    return
+  const checkPlayMode = async () => {
+    const element = await SpinQuery.select(
+      [...autoPlayControls.disable, ...autoPlayControls.enable].join(',')
+    )
+    if (!element) {
+      return
+    }
+    const shouldChecked = autoPlayControls.enable.some(selector => element.matches(selector)) && disableConditions.every(condition => !Boolean(condition()))
+    const checked = isChecked(element)
+    console.log(checked, shouldChecked, element)
+    if (shouldChecked !== checked) {
+      element.click()
+    }
   }
-  const shouldChecked = autoPlayControls.enable.some(selector => element.matches(selector))
-  const checked = isChecked(element)
-  console.log(checked, shouldChecked, element)
-  if (shouldChecked !== checked) {
-    element.click()
-  }
+  Observer.videoChange(() => {
+    const video = dq('.bilibili-player-video video') as HTMLVideoElement
+    checkPlayMode()
+    video?.addEventListener('ended', checkPlayMode)
+  })
 })()
