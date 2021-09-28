34442bfc76d29f28d349f72eab7acf0c93e45434
the1812 <the1812@outlook.com>
2021-09-28 23:25:38 +0800

    Fix layout in wide screen

--- src/style/simplify-home/simplify-home.vue
+++ src/style/simplify-home/simplify-home.vue
@@ -22,10 +22,12 @@ export default {
 <style lang="scss">
 .simplify-home {
   padding: 32px 72px 0;
+  margin: auto;
   display: flex;
   align-items: flex-start;
   justify-content: center;
   min-height: 100vh;
+  max-width: 2000px;
   --simplify-home-background: #f4f4f4;
   background-color: var(--simplify-home-background);
   font-size: 12px;
