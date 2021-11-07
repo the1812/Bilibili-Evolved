94630ea7b5cbf8fb1e05fca228e0a775fd63c528
the1812 <the1812@outlook.com>
2021-11-07 15:52:28 +0800

    Add feature: change update urls

---
+++ registry/lib/components/utils/change-update-urls/index.ts
@@ -0,0 +1,12 @@
+import { ComponentMetadata } from '@/components/types'
+
+export const component: ComponentMetadata = {
+  name: 'changeUpdateUrls',
+  displayName: '更新链接替换',
+  description: '批量更换已安装功能的更新链接的分支, 对本地安装的功能无效.',
+  entry: none,
+  tags: [componentsTags.utils],
+  widget: {
+    component: () => import('./Widget.vue').then(m => m.default),
+  },
+}
