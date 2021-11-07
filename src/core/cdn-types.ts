defaafdd4f04f6a2742dfa7b61768dcc2893db20
the1812 <the1812@outlook.com>
2021-11-07 15:38:39 +0800

    Use direct raw CDN (#2409)

--- src/core/cdn-types.ts
+++ src/core/cdn-types.ts
@@ -7,5 +7,5 @@ const defaultOwner = 'the1812'
 /** 根据分支名和仓库 owner 检索 CDN 链接 */
 export const cdnRoots: Record<CdnTypes, (branch: string, owner?: string) => string> = {
   jsDelivr: (branch, owner) => `https://cdn.jsdelivr.net/gh/${owner || defaultOwner}/Bilibili-Evolved@${branch}/`,
-  GitHub: (branch, owner) => `https://github.com/${owner || defaultOwner}/Bilibili-Evolved/raw/${branch}/`,
+  GitHub: (branch, owner) => `https://raw.githubusercontent.com/${owner || defaultOwner}/Bilibili-Evolved/${branch}/`,
 }
