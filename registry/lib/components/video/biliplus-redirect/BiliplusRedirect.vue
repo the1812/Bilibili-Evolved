fba53a0485f5f3d28595b158e6e9a99a357c68b2
the1812 <the1812@outlook.com>
2021-11-14 22:08:55 +0800

    Fix biliplus redirect url (#2322)

--- registry/lib/components/video/biliplus-redirect/BiliplusRedirect.vue
+++ registry/lib/components/video/biliplus-redirect/BiliplusRedirect.vue
@@ -8,6 +8,46 @@
 import { videoChange } from '@/core/observer'
 import { DefaultWidget } from '@/ui'
 
+const videoRegex = /\/(video|medialist\/play)\/([^\/]+\/)?(av[\d]+|BV.+)/i
+interface BiliplusRedirectProvider {
+  condition: () => boolean
+  getUrl: (host: string, updateUrl: (url: string) => void) => string
+}
+const redirectProviders: BiliplusRedirectProvider[] = [
+  {
+    condition: () => window.location.host === 'space.bilibili.com',
+    getUrl: host => document.URL.replace('space.bilibili.com/', `${host}/space/`),
+  },
+  {
+    condition: () => window.location.host === 'space.bilibili.com',
+    getUrl: host => document.URL.replace('space.bilibili.com/', `${host}/space/`),
+  },
+  {
+    condition: () => document.URL.includes('/bangumi/play'),
+    getUrl: (host, updateUrl) => {
+      videoChange(() => {
+        const aid = unsafeWindow.aid || (() => {
+          const link = document.querySelector('.av-link,.info-sec-av') as HTMLElement
+          return link.innerText.replace(/[aAvV]/g, '')
+        })()
+        const url = `https://${host}/video/av${aid}/`
+        if (document.URL !== url) {
+          updateUrl(url)
+        } else {
+          updateUrl('')
+        }
+      })
+      return `https://${host}${window.location.pathname}${window.location.search}`
+    },
+  },
+  {
+    condition: () => videoRegex.test(document.URL),
+    getUrl: host => {
+      const id = document.URL.match(videoRegex)[3]
+      return `https://${host}/video/${id}/`
+    },
+  },
+]
 export default Vue.extend({
   components: {
     DefaultWidget,
@@ -19,26 +59,10 @@ export default Vue.extend({
   },
   created() {
     const host = 'www.biliplus.com'
-    const videoRegex = /\/video\/(av[\d]+|BV.+)/i
-    if (window.location.host === 'space.bilibili.com') {
-      this.url = document.URL.replace('space.bilibili.com/', `${host}/space/`)
-    } else if (document.URL.includes('/bangumi/play')) {
-      this.url = `https://${host}${window.location.pathname}${window.location.search}`
-      videoChange(() => {
-        const aid = unsafeWindow.aid || (() => {
-          const link = document.querySelector('.av-link,.info-sec-av') as HTMLElement
-          return link.innerText.replace(/[aAvV]/g, '')
-        })()
-        const url = `https://${host}/video/av${aid}/`
-        if (document.URL !== url) {
-          this.href = url
-        } else {
-          this.href = ''
-        }
-      })
-    } else if (videoRegex.test(document.URL)) {
-      const [, id] = document.URL.match(videoRegex)
-      this.url = `https://${host}/video/${id}/`
+    const updateUrl = (url: string) => (this.url = url)
+    const provider = redirectProviders.find(p => p.condition())
+    if (provider) {
+      updateUrl(provider.getUrl(host, updateUrl))
     } else {
       videoChange(() => {
         this.url = document.URL.replace(window.location.host, host)
