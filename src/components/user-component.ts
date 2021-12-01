499f299a0c518e3ebed50966e272bdcf785d136a
the1812 <the1812@outlook.com>
2021-12-01 13:26:54 +0800

    Fix options merging (#2501)

--- src/components/user-component.ts
+++ src/components/user-component.ts
@@ -1,4 +1,4 @@
-import { ComponentSettings, componentToSettings } from '@/core/settings'
+import { componentToSettings } from '@/core/settings'
 import { getBuiltInComponents } from './built-in-components'
 import {
   ComponentMetadata, componentsMap,
@@ -37,16 +37,11 @@ export const installComponent = async (code: string) => {
   if (existingComponent) {
     existingComponent.code = code
     existingComponent.metadata = userMetadata
-    const existingOptions = existingComponent.settings.options
-    const newSettings: ComponentSettings = lodash.defaultsDeep(
-      existingComponent.settings,
-      componentToSettings(component),
+    const defaultSettings = componentToSettings(component)
+    lodash.defaultsDeep(
+      existingComponent.settings.options,
+      lodash.pickBy(defaultSettings.options, value => !Array.isArray(value)),
     )
-    Object.entries(existingOptions).forEach(([name, option]) => {
-      if (Array.isArray(option)) {
-        newSettings.options[name] = option
-      }
-    })
     return {
       metadata: component,
       message: `已更新组件'${component.displayName}', 刷新后生效`,
