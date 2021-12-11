a4b5e511d2aca20699647825e52bf0e87af32ddb
ifwlzs <49548316+ifwlzs@users.noreply.github.com>
2021-12-11 14:12:07 +0800

    Create purge_cache.py

---
+++ .github/scripts/purge_cache.py
@@ -0,0 +1,21 @@
+import os, requests
+
+
+def get_file_list(folder_dir: str):
+    dir_list = os.listdir(folder_dir)
+    f_list = []
+    for d in dir_list:
+        sub_dir = os.path.join(folder_dir, d)
+        if os.path.isfile(sub_dir):
+            f_list.append(d)
+    return f_list
+
+
+if __name__ == '__main__':
+    work_space = os.environ.get('GITHUB_WORKSPACE')
+    file_list = get_file_list(work_space)
+    for file in file_list:
+        # https://cdn.jsdelivr.net/gh/the1812/Bilibili-Evolved@preview/dist/bilibili-evolved.preview.user.js
+        url = f'https://purge.jsdelivr.net/gh/the1812/Bilibili-Evolved/{file}'
+        print(url)
+        print(requests.get(url).text)
