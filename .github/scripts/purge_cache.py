7c7f21488550463b0cee42285e06a096df6e9c58
ifwlzs <49548316+ifwlzs@users.noreply.github.com>
2021-12-11 14:24:35 +0800

    Update purge_cache.py

--- .github/scripts/purge_cache.py
+++ .github/scripts/purge_cache.py
@@ -15,7 +15,12 @@ if __name__ == '__main__':
     work_space = os.environ.get('GITHUB_WORKSPACE')
     file_list = get_file_list(work_space)
     for file in file_list:
-        # https://cdn.jsdelivr.net/gh/the1812/Bilibili-Evolved@master/dist/bilibili-evolved.preview.user.js
+        # https://cdn.jsdelivr.net/gh/the1812/Bilibili-Evolved@master/dist/*
+        url = f'https://purge.jsdelivr.net/gh/the1812/Bilibili-Evolved/dist/{file}'
+        print(url)
+        print(requests.get(url).text)
+         # https://cdn.jsdelivr.net/gh/the1812/Bilibili-Evolved@master/*
         url = f'https://purge.jsdelivr.net/gh/the1812/Bilibili-Evolved/{file}'
         print(url)
         print(requests.get(url).text)
+        
