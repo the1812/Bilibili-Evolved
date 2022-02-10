546e99f807022500d5b90bb5ef06cbc978a9d763
timongh <46739861+timongh@users.noreply.github.com>
2022-02-11 04:16:52 +0800

    Add the missing url rule: upListUrls (#2986)

--- src/core/utils/urls.ts
+++ src/core/utils/urls.ts
@@ -9,10 +9,15 @@ export const watchlaterUrls = [
 export const favoriteListUrls = [
   '//www.bilibili.com/medialist/play/ml',
 ]
+/** UP 主视频连播页面 */
+export const upListUrls = [
+  /\/\/www\.bilibili\.com\/medialist\/play\/\d+/,
+]
 /** 合集类页面 */
 export const mediaListUrls = [
   ...watchlaterUrls,
   ...favoriteListUrls,
+  ...upListUrls,
 ]
 /** 含有普通视频的页面 */
 export const videoUrls = [
