db1726e400eb57b8560a14897a615bdb9f5787d5
timongh <46739861+timongh@users.noreply.github.com>
2022-02-11 04:29:54 +0800

    Separate out bnjUrls

--- src/core/utils/urls.ts
+++ src/core/utils/urls.ts
@@ -19,10 +19,14 @@ export const mediaListUrls = [
   ...favoriteListUrls,
   ...upListUrls,
 ]
+/** 拜年纪页面 */
+export const bnjUrls = [
+  /\/\/www\.bilibili\.com\/festival\/(\d+)bnj/,
+]
 /** 含有普通视频的页面 */
 export const videoUrls = [
   '//www.bilibili.com/video/',
-  /\/\/www\.bilibili\.com\/festival\/(\d+)bnj/,
+  ...bnjUrls,
   ...mediaListUrls,
 ]
 /** 含有番剧的页面 */
