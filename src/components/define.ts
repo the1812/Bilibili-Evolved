6ef7c203061c71ca24965db11d74bffe27f15664
timongh <46739861+timongh@users.noreply.github.com>
2022-03-18 21:08:47 +0800

    Add comments to define api

--- src/components/define.ts
+++ src/components/define.ts
@@ -11,14 +11,17 @@ export type OptionsOfMetadata<M extends OptionsMetadata> = (
   M extends OptionsMetadata<infer O> ? O : never
 )
 
+/** 定义单个 OptionMetadata */
 export const defineOptionMetadata = <T>(
   m: OptionMetadata<T>,
 ): OptionMetadata<T> => m
 
+/** 单独定义 OptionsMetadata */
 export const defineOptionsMetadata = <
   O extends UnknownOptions
 >(m: OptionsMetadata<O>): OptionsMetadata<O> => m
 
+/** 定义组件 */
 export const defineComponentMetadata = <
   Om extends OptionalOptionsMetadata
 >(m: ComponentMetadata<Om>): ComponentMetadata<Om> => m
