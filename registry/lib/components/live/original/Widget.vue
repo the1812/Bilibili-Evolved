8d83d55fe341343abb05173e158c5c5c0552c29b
the1812 <the1812@outlook.com>
2022-03-20 11:01:21 +0800

    Add originalLiveroom (#2133)

---
+++ registry/lib/components/live/original/Widget.vue
@@ -0,0 +1,28 @@
+<template>
+  <a :href="url" target="_blank" tabindex="-1">
+    <DefaultWidget
+      name="返回原版直播间"
+      icon="mdi-arrow-left-circle-outline"
+    />
+  </a>
+</template>
+<script lang="ts">
+import { DefaultWidget } from '@/ui'
+
+export default Vue.extend({
+  components: {
+    DefaultWidget,
+  },
+  data() {
+    const match = document.URL.match(/^https:\/\/live\.bilibili\.com\/([\d]+)/)
+    if (!match) {
+      return {
+        href: document.URL,
+      }
+    }
+    return {
+      href: `https://live.bilibili.com/blanc/${match[1]}`,
+    }
+  },
+})
+</script>
