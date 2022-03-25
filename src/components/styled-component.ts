6c01e0a1d7b15d27d5b1c493df3cff285e2a3e37
timongh <46739861+timongh@users.noreply.github.com>
2022-03-25 17:18:15 +0800

    Improve type annotations in styled-component.ts

--- src/components/styled-component.ts
+++ src/components/styled-component.ts
@@ -1,34 +1,32 @@
-import { none } from '@/core/utils'
-import { ComponentEntry, ComponentMetadata } from './component'
+import { ComponentEntry, ComponentMetadata, OptionalOptionsMetadata } from './component'
 
 /**
  * 创建一个自动添加指定样式的组件入口函数
  * @param styleImport 动态导入样式的函数
  * @param entry 组件入口函数
  */
-export const styledComponentEntry = (
+export const styledComponentEntry = <Om extends OptionalOptionsMetadata, T>(
   styleImport: () => Promise<{ default: string }>,
-  entry: ComponentEntry,
-): ComponentEntry => (
-  async context => {
-    const { default: style } = await styleImport()
-    const { addStyle } = await import('@/core/style')
-    addStyle(style, context.metadata.name)
-    return entry(context)
-  }
-)
+  entry: ComponentEntry<Om, T>,
+): ComponentEntry<Om, T> => async context => {
+  const { default: style } = await styleImport()
+  const { addStyle } = await import('@/core/style')
+  addStyle(style, context.metadata.name)
+  return entry(context)
+}
 
 /**
  * 创建仅切换样式的组件`entry`, `reload`和`unload`, 展开至组件定义中即可, 也可以提供可选的组件入口函数
+ * @param name 组件名称
  * @param styleImport 动态导入样式的函数
  * @param entry 组件入口函数
  */
-export const toggleStyle = (
+export const toggleStyle = <Om extends OptionalOptionsMetadata>(
   name: string,
   styleImport: () => Promise<{ default: string }>,
-  entry: ComponentEntry = none,
-): Pick<ComponentMetadata, 'name' | 'entry' | 'reload' | 'unload'> => {
-  let styleElement: HTMLStyleElement = null
+  entry: ComponentEntry<Om> = none,
+): Pick<ComponentMetadata<Om>, 'name' | 'entry' | 'reload' | 'unload'> => {
+  let styleElement: HTMLStyleElement | null = null
   const styleEntry = async () => {
     if (styleElement) {
       return
@@ -39,9 +37,8 @@ export const toggleStyle = (
   }
   return {
     name,
-    entry: context => (
-      styleEntry().then(() => entry(context))
-    ),
+    entry: context => styleEntry()
+      .then(() => entry(context)),
     reload: styleEntry,
     unload: () => {
       styleElement?.remove()
