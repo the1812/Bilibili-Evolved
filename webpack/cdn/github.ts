7fe8df162533c5736a627ce4b17c74e15eb26d39
the1812 <the1812@outlook.com>
2022-06-28 08:44:51 +0800

    Fix library CDN url

--- webpack/cdn/github.ts
+++ webpack/cdn/github.ts
@@ -9,8 +9,8 @@ export const github: CdnConfig = {
   previewClient: `https://${host}/${owner}/Bilibili-Evolved/preview/dist/bilibili-evolved.preview.user.js`,
   library: {
     lodash: `https://${host}/lodash/lodash/4.17.21/dist/lodash.min.js`,
-    protobuf: `https://${host}/protobufjs/protobuf.js/6.10.1/dist/light/protobuf.min.js`,
-    jszip: `https://${host}/Stuk/jszip/3.7.1/dist/jszip.min.js`,
+    protobuf: `https://${host}/protobufjs/protobuf.js/v6.10.1/dist/light/protobuf.min.js`,
+    jszip: `https://${host}/Stuk/jszip/v3.7.1/dist/jszip.min.js`,
     sortable: `https://${host}/SortableJS/Sortable/1.14.0/Sortable.min.js`,
     mdi: `https://${owner}.github.io/Bilibili-Evolved/static/mdi/mdi.css`,
   },
