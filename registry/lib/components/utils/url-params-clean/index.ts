91e7d7ec3d1654d133f0dbdffc8def80e05860c1
EzraRT <platoongo@gmail.com>
2022-08-17 20:29:29 +0800

    ESLint fix

--- registry/lib/components/utils/url-params-clean/index.ts
+++ registry/lib/components/utils/url-params-clean/index.ts
@@ -47,9 +47,9 @@ const entry = async () => {
     'spm',
     'hotRank',
     '-Arouter',
-    'vd_source',    
+    'vd_source',
     'is_story_h5',
-    'share_from'
+    'share_from',
   ]
   const [blockParams] = registerAndGetData('urlParamsClean.params', builtInBlockParams)
   const builtInSiteSpecifiedParams = [
