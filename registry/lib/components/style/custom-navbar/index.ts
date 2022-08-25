54eb5a04bc9d6bfdd3d9eaee3da8805dd2c663f3
the1812 <the1812@outlook.com>
2022-08-25 21:59:12 +0800

    Use markdown file for description

--- registry/lib/components/style/custom-navbar/index.ts
+++ registry/lib/components/style/custom-navbar/index.ts
@@ -76,11 +76,6 @@ const options = defineOptionsMetadata({
 export const component = defineComponentMetadata({
   name: 'customNavbar',
   displayName: '自定义顶栏',
-  description: `
-使用脚本提供的顶栏替换原版的主站顶栏 (非主站不替换), 启用后还可以在反广告插件 (例如 ABP) 中配置禁止原版顶栏的消息 iframe 以提升性能:
-- *://message.bilibili.com/pages/nav/index_new_pc_sync
-- *://message.bilibili.com/pages/nav/index_new_sync
-  `.trim(),
   entry,
   tags: [
     componentsTags.style,
