b9dfb5e30d353a3d993b8c2c9a05a1be17ccaf7c
the1812 <the1812@outlook.com>
2022-08-30 22:03:30 +0800

    Improve expandDanmakuList - Add option maxDanmakuCount (#2972) - 3.x player support (#3187)

--- registry/lib/components/video/danmaku/expand/index.ts
+++ registry/lib/components/video/danmaku/expand/index.ts
@@ -2,11 +2,37 @@ import { ComponentEntry, ComponentMetadata } from '@/components/types'
 import { videoChange } from '@/core/observer'
 import { select } from '@/core/spin-query'
 import { matchUrlPattern } from '@/core/utils'
+import { useScopedConsole } from '@/core/utils/log'
 import { mediaListUrls, videoAndBangumiUrls } from '@/core/utils/urls'
 
+const getDanmakuCount = async () => {
+  const countElement = await select('.bilibili-player-video-info-danmaku-number, .bpx-player-video-info-dm')
+  /*
+    3.x: 已装填 n 条弹幕
+    2.x: n
+  */
+  const countText = countElement?.textContent ?? ''
+  const countMatch = countText.match(/\d+/)
+  if (!countMatch) {
+    return null
+  }
+  const count = parseInt(countMatch[0])
+  if (Number.isNaN(count)) {
+    return null
+  }
+  return count
+}
+
 const entry: ComponentEntry = async ({ settings: { options } }) => {
+  const console = useScopedConsole('展开弹幕列表')
   videoChange(async () => {
     if (mediaListUrls.some(url => matchUrlPattern(url)) && options.ignoreMediaList) {
+      return
+    }
+    const danmakuCount = await getDanmakuCount()
+    console.log(`当前弹幕量: ${danmakuCount}`)
+    if (danmakuCount !== null && danmakuCount > options.maxDanmakuCount) {
+      console.log(`超过了最大弹幕数量 ${options.maxDanmakuCount}, 跳过展开`)
       return
     }
     const danmakuBox = await select('.bui-collapse-wrap')
@@ -23,12 +49,16 @@ export const component: ComponentMetadata = {
   entry,
   tags: [componentsTags.video],
   description: {
-    'zh-CN': '每次加载视频时自动展开弹幕列表.',
+    'zh-CN': '每次加载视频时自动展开弹幕列表, 如果弹幕装载量超过 `最大弹幕数量`, 则不展开以免对页面造成卡顿.',
   },
   options: {
     ignoreMediaList: {
       defaultValue: true,
       displayName: '合集类页面不自动展开',
+    },
+    maxDanmakuCount: {
+      defaultValue: 500,
+      displayName: '最大弹幕数量',
     },
   },
   urlInclude: [
