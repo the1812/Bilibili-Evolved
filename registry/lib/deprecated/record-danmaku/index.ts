ad85c84cf3638e2ca3ffe75d5ee4f7568e6d724a
timongh <46739861+timongh@users.noreply.github.com>
2022-09-16 14:33:37 +0800

    Change components in deprecated video to define api

--- registry/lib/deprecated/record-danmaku/index.ts
+++ registry/lib/deprecated/record-danmaku/index.ts
@@ -1,19 +1,17 @@
-import { ComponentMetadata } from '@/components/types'
+import { defineComponentMetadata } from '@/components/define'
 import { none } from '@/core/utils'
 import { liveUrls } from '@/core/utils/urls'
 
-export const component: ComponentMetadata = {
+export const component = defineComponentMetadata({
   name: 'recordLiveDanmaku',
   displayName: '直播弹幕记录器',
   description: {
     'zh-CN': '在功能中添加直播弹幕记录器, 可以记录直播弹幕并导出XML.',
   },
   entry: none,
-  tags: [
-    componentsTags.live,
-  ],
+  tags: [componentsTags.live],
   widget: {
     component: () => import('./RecordDanmaku.vue').then(m => m.default),
   },
   urlInclude: liveUrls,
-}
+})
