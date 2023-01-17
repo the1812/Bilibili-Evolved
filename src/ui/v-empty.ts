e3a406c6b71e146065c901d5e0e66bda2e48fb27
Ethkuil <hobartlin23@gmail.com>
2023-01-17 19:50:32 +0800

    Update default empty content (fix #3932)

--- src/ui/v-empty.ts
+++ src/ui/v-empty.ts
@@ -1,6 +1,6 @@
 import { registerAndGetData } from '@/plugins/data'
 
-const defaultEmptyContent = '空空如也哦 =￣ω￣='
+const defaultEmptyContent = '空空如也哦 =￣ω￣=  全新安装的脚本没有任何功能，你可以在设置面板 左下角 点击进入「组件」面板，再点击「在线」，从在线功能库任意安装自己想要的功能~'
 export const [emptyContent] = registerAndGetData('vEmpty', {
   content: defaultEmptyContent,
 })
