382c272a7ce69440a5bc4205cf9c0291579a1641
the1812 <the1812@outlook.com>
2023-02-04 17:57:33 +0800

    Fix image resolution for srcset (fix #3894)

--- registry/lib/components/utils/image-resolution/resolution.ts
+++ registry/lib/components/utils/image-resolution/resolution.ts
@@ -28,6 +28,10 @@ export const imageResolution = async (dpi: number, element: HTMLElement) => {
     if (excludeSelectors.some(it => element.matches(it))) {
       return
     }
+    // 带 , 的 srcset 不处理
+    if (value.includes(',')) {
+      return
+    }
     const match = value.match(resizeRegex)
     if (!match) {
       return
@@ -55,6 +59,10 @@ export const imageResolution = async (dpi: number, element: HTMLElement) => {
     replaceSource(
       e => e.getAttribute('src'),
       (e, v) => e.setAttribute('src', v),
+    )
+    replaceSource(
+      e => e.getAttribute('srcset'),
+      (e, v) => e.setAttribute('srcset', v),
     )
     replaceSource(
       e => e.style.backgroundImage,
