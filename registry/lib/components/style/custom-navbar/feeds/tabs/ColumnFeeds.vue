d49d7b890991375e9d3ff068b3b9c0a3dbbdfe14
timongh <46739861+timongh@users.noreply.github.com>
2023-02-08 03:19:44 +0800

    Replace `nextPageMixin` to vue composition style

--- registry/lib/components/style/custom-navbar/feeds/tabs/ColumnFeeds.vue
+++ registry/lib/components/style/custom-navbar/feeds/tabs/ColumnFeeds.vue
@@ -1,3 +1,34 @@
+<script setup lang="ts">
+import { ScrollTrigger, VEmpty, VLoading } from '@/ui'
+import { feedsCardTypes } from '@/components/feeds/api'
+import type { ColumnCard as ColumnCardData } from '@/components/feeds/column-card'
+import ColumnCard from '@/components/feeds/ColumnCard.vue'
+import { isNewID } from '@/components/feeds/notify'
+
+import { useNextPage } from './next-page'
+
+const { loading, cards, hasMorePage, nextPage } = useNextPage(
+  feedsCardTypes.column,
+  (card: any): ColumnCardData => {
+    const cardJson = JSON.parse(card.card)
+    return {
+      id: card.desc.dynamic_id_str,
+      cvID: cardJson.id,
+      title: cardJson.title,
+      upName: cardJson.author.name,
+      upFaceUrl: cardJson.author.face,
+      upID: cardJson.author.mid,
+      description: cardJson.summary,
+      covers: cardJson.image_urls,
+      originalCovers: cardJson.origin_image_urls,
+      get new() {
+        return isNewID(this.id)
+      },
+    }
+  },
+)
+</script>
+
 <template>
   <div class="column-feeds">
     <VLoading v-if="loading"></VLoading>
@@ -10,40 +41,7 @@
     </template>
   </div>
 </template>
-<script lang="ts">
-import { defineComponent } from 'vue'
-import { feedsCardTypes } from '@/components/feeds/api'
-import type { ColumnCard } from '@/components/feeds/column-card'
-import ColumnCardComponent from '@/components/feeds/ColumnCard.vue'
-import { isNewID } from '@/components/feeds/notify'
 
-import { nextPageMixin } from './next-page'
-
-export default defineComponent({
-  components: {
-    ColumnCard: ColumnCardComponent,
-  },
-  mixins: [
-    nextPageMixin(feedsCardTypes.column, (card: any) => {
-      const cardJson = JSON.parse(card.card)
-      return {
-        id: card.desc.dynamic_id_str,
-        cvID: cardJson.id,
-        title: cardJson.title,
-        upName: cardJson.author.name,
-        upFaceUrl: cardJson.author.face,
-        upID: cardJson.author.mid,
-        description: cardJson.summary,
-        covers: cardJson.image_urls,
-        originalCovers: cardJson.origin_image_urls,
-        get new() {
-          return isNewID(this.id)
-        },
-      } as ColumnCard
-    }),
-  ],
-})
-</script>
 <style lang="scss">
 .column-feeds {
   display: flex;
