c5eeb6335cdc91cb22557f64c0fa3509c5aedb18
timongh <46739861+timongh@users.noreply.github.com>
2023-02-09 17:23:56 +0800

    Use type `Component` as vue component in reimu/index.ts

--- registry/lib/plugins/v-loading/reimu/index.ts
+++ registry/lib/plugins/v-loading/reimu/index.ts
@@ -1,4 +1,6 @@
-import type { Executable, ImportedVueComponent } from '@/core/common-types'
+import type { Component } from 'vue'
+import { defineAsyncComponent } from 'vue'
+
 import type { PluginMetadata } from '@/plugins/plugin'
 
 export const plugin: PluginMetadata = {
@@ -7,8 +9,8 @@ export const plugin: PluginMetadata = {
   description:
     '用灵梦油库里代替脚本的所有 "加载中" 提示, 油库里素材来自[东方我乐多从志](https://cn.touhougarakuta.com/).',
   setup: ({ addData }) => {
-    addData('vLoading', (config: { content: Executable<ImportedVueComponent> | string }) => {
-      config.content = () => import('./ReimuLoading.vue').then(m => m.default)
+    addData('vLoading', (config: { content: Component | string }) => {
+      config.content = defineAsyncComponent(() => import('./ReimuLoading.vue'))
     })
   },
 }
