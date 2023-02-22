ba7b7807e0d12453dad9b1796d7caa51ebbac6c5
timongh <46739861+timongh@users.noreply.github.com>
2023-02-22 22:44:59 +0800

    Change comments of `SwitchComponentWrapper`

--- src/components/switch-options.ts
+++ src/components/switch-options.ts
@@ -272,9 +272,9 @@ export type SwitchComponentMetadata<
  * 用于包装组件元数据，根据配置生成一系列开关
  *
  * 修改内容包括
- * - 注入一系列选项，具体细节参考 {@link SwitchOptions}
+ * - 注入一系列 options，具体细节参考 {@link SwitchOptions}
  * - 若传入组件未定义 Widget，则会注入一个用于控制各开关的 Widget。此 Widget 的功能与注入组件的 options 功能相同
- * - 被包装后的元数据，其 `entry` 函数是原函数的包装，用于实现相关功能
+ * - 包装 `entry`，用于实现相关功能
  *
  * 该函数会直接修改传入参数自身，并将其返回。
  *
