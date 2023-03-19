71818290924c231aa3459b2d73e61ae1beb0bd69
z503722728 <503722728@qq.com>
2023-03-19 18:35:55 +0800

    关灯增强 添加星空特效

--- registry/dist/components/video/player/auto-light.js
+++ registry/dist/components/video/player/auto-light.js
@@ -1,1 +1,201 @@
-!function(e,t){"object"==typeof exports&&"object"==typeof module?module.exports=t():"function"==typeof define&&define.amd?define([],t):"object"==typeof exports?exports["video/player/auto-light"]=t():e["video/player/auto-light"]=t()}(globalThis,(()=>(()=>{"use strict";var e,t,o={642:e=>{e.exports=coreApis.componentApis.video.playerLight},605:e=>{e.exports=coreApis.utils}},r={};function n(e){var t=r[e];if(void 0!==t)return t.exports;var i=r[e]={exports:{}};return o[e](i,i.exports,n),i.exports}t=Object.getPrototypeOf?e=>Object.getPrototypeOf(e):e=>e.__proto__,n.t=function(o,r){if(1&r&&(o=this(o)),8&r)return o;if("object"==typeof o&&o){if(4&r&&o.__esModule)return o;if(16&r&&"function"==typeof o.then)return o}var i=Object.create(null);n.r(i);var a={};e=e||[null,t({}),t([]),t(t)];for(var s=2&r&&o;"object"==typeof s&&!~e.indexOf(s);s=t(s))Object.getOwnPropertyNames(s).forEach((e=>a[e]=()=>o[e]));return a.default=()=>o,n.d(i,a),i},n.d=(e,t)=>{for(var o in t)n.o(t,o)&&!n.o(e,o)&&Object.defineProperty(e,o,{enumerable:!0,get:t[o]})},n.o=(e,t)=>Object.prototype.hasOwnProperty.call(e,t),n.r=e=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})};var i={};return(()=>{n.d(i,{component:()=>s});const e=coreApis.componentApis.define,t=coreApis.componentApis.video.playerAgent,o=coreApis.observer,r=coreApis.utils.urls;let a;const s=(0,e.defineComponentMetadata)({name:"playerAutoLight",displayName:"播放时自动关灯",urlInclude:r.allVideoUrls,tags:[componentsTags.video],description:{"zh-CN":"在视频播放时自动关灯, 暂停或结束时再自动打开."},entry:async()=>{const{isEmbeddedPlayer:e}=await Promise.resolve().then(n.t.bind(n,605,23)),{lightOn:r,lightOff:i}=await Promise.resolve().then(n.t.bind(n,642,23));e()||(0,o.videoChange)((async()=>{if(null!=a){const e=await a.query.video.element();e.removeEventListener("ended",r),e.removeEventListener("pause",r),e.removeEventListener("play",i)}a=t.playerAgent;const e=await a.query.video.element();a.isAutoPlay()&&i(),e.addEventListener("ended",r),e.addEventListener("pause",r),e.addEventListener("play",i)}))},commitHash:"fd37f297fdb2985b25a97e154e00dd39090a4da2",coreVersion:"2.5.2"})})(),i=i.component})()));
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["video/player/auto-light"] = factory();
+	else
+		root["video/player/auto-light"] = factory();
+})(globalThis, () => {
+return /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./registry/lib/components/video/player/auto-light/index.ts":
+/*!******************************************************************!*\
+  !*** ./registry/lib/components/video/player/auto-light/index.ts ***!
+  \******************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"component\": () => (/* binding */ component)\n/* harmony export */ });\n/* harmony import */ var _components_define__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @/components/define */ \"@/components/define\");\n/* harmony import */ var _components_define__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_components_define__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _components_video_player_agent__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @/components/video/player-agent */ \"@/components/video/player-agent\");\n/* harmony import */ var _components_video_player_agent__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_components_video_player_agent__WEBPACK_IMPORTED_MODULE_1__);\n/* harmony import */ var _components_video_player_light__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @/components/video/player-light */ \"@/components/video/player-light\");\n/* harmony import */ var _components_video_player_light__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_components_video_player_light__WEBPACK_IMPORTED_MODULE_2__);\n/* harmony import */ var _core_observer__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @/core/observer */ \"@/core/observer\");\n/* harmony import */ var _core_observer__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(_core_observer__WEBPACK_IMPORTED_MODULE_3__);\n/* harmony import */ var _core_utils_urls__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @/core/utils/urls */ \"@/core/utils/urls\");\n/* harmony import */ var _core_utils_urls__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(_core_utils_urls__WEBPACK_IMPORTED_MODULE_4__);\n\n\n\n\n\nlet playerAgentInstance;\nconst CreateAnim = () => {\n  let biliMainHeader = document.getElementById(\"biliMainHeader\");\n  if (!biliMainHeader) return;\n  let mstars1 = document.createElement(\"div\");\n  mstars1.id = \"mstars1\";\n  let mstars2 = document.createElement(\"div\");\n  mstars2.id = \"mstars2\";\n  biliMainHeader.appendChild(mstars1);\n  biliMainHeader.appendChild(mstars2);\n\n  // 添加一段css 样式到document最后\n  let style = document.createElement(\"style\");\n  // generate random stars\n  function generate(numCtrl) {\n    let star = '';\n    let max = window.innerWidth * window.innerHeight;\n    for (let i = 0; i < max / numCtrl; i++) {\n      star += `${Math.floor(Math.random() * window.innerWidth * 1.5)}px ${Math.floor(Math.random() * (window.innerHeight + 2000))}px #FFF,`;\n    }\n    star += `${Math.floor(Math.random() * window.innerWidth * 1.5)}px ${Math.floor(Math.random() * (window.innerHeight + 2000))}px #FFF;`;\n    return star;\n  }\n  let starNumCtl = 400;\n  style.innerHTML = `\n  #mstars1{z-index: 1009;position: fixed;left:0px; width:1px;height:1px;background:transparent;box-shadow:${generate(starNumCtl)};animation:animStar 50s linear infinite}\n  #mstars1:after{content:\" \";position:fixed;left:0px;top:0px;width:1px;height:1px;background:transparent;box-shadow:${generate(starNumCtl * 2)}}\n  #mstars2{z-index: 1009;position: fixed;left:0px;width:2px;height:2px;background:transparent;box-shadow:${generate(starNumCtl * 4)};animation:animStar 100s linear infinite}\n  #mstars2:after{content:\" \";position:fixed;left:0px;top:0px;width:2px;height:2px;background:transparent;box-shadow:${generate(starNumCtl * 8)}}\n  @keyframes animStar{from{transform:translateY(${-200}px)}to{transform:translateY(${-2200}px)}}\n  `;\n  document.body.appendChild(style);\n};\nconst StarAnim = on => {\n  //查找id mstars1 的div\n  let mstars1 = document.getElementById(\"mstars1\");\n  let mstars2 = document.getElementById(\"mstars2\");\n  //如果没有找到id biliMainHeader 的div创建2个id为 mstars1 mstars2 的div\n  if (on) {\n    if (mstars1 == null) {\n      CreateAnim();\n      mstars1 = document.getElementById(\"mstars1\");\n      mstars2 = document.getElementById(\"mstars2\");\n    }\n    //设置mstars1 mstars2 visible 为true\n    mstars1.style.visibility = \"visible\";\n    mstars2.style.visibility = \"visible\";\n  } else {\n    if (mstars1 != null) {\n      mstars1.style.visibility = \"hidden\";\n      mstars2.style.visibility = \"hidden\";\n    }\n  }\n};\nlet lightOn2 = () => {\n  (0,_components_video_player_light__WEBPACK_IMPORTED_MODULE_2__.lightOn)();\n  StarAnim(false);\n};\nlet lightOff2 = () => {\n  (0,_components_video_player_light__WEBPACK_IMPORTED_MODULE_2__.lightOff)();\n  StarAnim(true);\n};\nconst component = (0,_components_define__WEBPACK_IMPORTED_MODULE_0__.defineComponentMetadata)({\n  name: 'playerAutoLight',\n  displayName: '播放时自动关灯',\n  urlInclude: _core_utils_urls__WEBPACK_IMPORTED_MODULE_4__.allVideoUrls,\n  tags: [componentsTags.video],\n  description: {\n    'zh-CN': '在视频播放时自动关灯, 暂停或结束时再自动打开.'\n  },\n  entry: async () => {\n    const {\n      isEmbeddedPlayer\n    } = await Promise.resolve(/*! import() */).then(__webpack_require__.t.bind(__webpack_require__, /*! @/core/utils */ \"@/core/utils\", 23));\n    const {\n      lightOn,\n      lightOff\n    } = await Promise.resolve(/*! import() */).then(__webpack_require__.t.bind(__webpack_require__, /*! @/components/video/player-light */ \"@/components/video/player-light\", 23));\n    if (isEmbeddedPlayer()) {\n      return;\n    }\n    (0,_core_observer__WEBPACK_IMPORTED_MODULE_3__.videoChange)(async () => {\n      if (playerAgentInstance != null) {\n        const oldVideo = await playerAgentInstance.query.video.element();\n        oldVideo.removeEventListener('ended', lightOn2);\n        oldVideo.removeEventListener('pause', lightOn2);\n        oldVideo.removeEventListener('play', lightOff2);\n      }\n      playerAgentInstance = _components_video_player_agent__WEBPACK_IMPORTED_MODULE_1__.playerAgent;\n      const video = await playerAgentInstance.query.video.element();\n      if (playerAgentInstance.isAutoPlay()) {\n        lightOff2();\n      }\n      video.addEventListener('ended', lightOn2);\n      video.addEventListener('pause', lightOn2);\n      video.addEventListener('play', lightOff2);\n    });\n  },\n  commitHash: \"7991ac4fab1d03b125b3404b7d959c16e56d6917\",\n  coreVersion: \"2.6.3\"\n});\n\n//# sourceURL=webpack://@bevo/core/./registry/lib/components/video/player/auto-light/index.ts?");
+
+/***/ }),
+
+/***/ "@/components/define":
+/*!******************************************************!*\
+  !*** external ["coreApis","componentApis","define"] ***!
+  \******************************************************/
+/***/ ((module) => {
+
+module.exports = coreApis.componentApis.define;
+
+/***/ }),
+
+/***/ "@/components/video/player-agent":
+/*!*******************************************************************!*\
+  !*** external ["coreApis","componentApis","video","playerAgent"] ***!
+  \*******************************************************************/
+/***/ ((module) => {
+
+module.exports = coreApis.componentApis.video.playerAgent;
+
+/***/ }),
+
+/***/ "@/components/video/player-light":
+/*!*******************************************************************!*\
+  !*** external ["coreApis","componentApis","video","playerLight"] ***!
+  \*******************************************************************/
+/***/ ((module) => {
+
+module.exports = coreApis.componentApis.video.playerLight;
+
+/***/ }),
+
+/***/ "@/core/observer":
+/*!****************************************!*\
+  !*** external ["coreApis","observer"] ***!
+  \****************************************/
+/***/ ((module) => {
+
+module.exports = coreApis.observer;
+
+/***/ }),
+
+/***/ "@/core/utils/urls":
+/*!********************************************!*\
+  !*** external ["coreApis","utils","urls"] ***!
+  \********************************************/
+/***/ ((module) => {
+
+module.exports = coreApis.utils.urls;
+
+/***/ }),
+
+/***/ "@/core/utils":
+/*!*************************************!*\
+  !*** external ["coreApis","utils"] ***!
+  \*************************************/
+/***/ ((module) => {
+
+module.exports = coreApis.utils;
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/create fake namespace object */
+/******/ 	(() => {
+/******/ 		var getProto = Object.getPrototypeOf ? (obj) => (Object.getPrototypeOf(obj)) : (obj) => (obj.__proto__);
+/******/ 		var leafPrototypes;
+/******/ 		// create a fake namespace object
+/******/ 		// mode & 1: value is a module id, require it
+/******/ 		// mode & 2: merge all properties of value into the ns
+/******/ 		// mode & 4: return value when already ns object
+/******/ 		// mode & 16: return value when it's Promise-like
+/******/ 		// mode & 8|1: behave like require
+/******/ 		__webpack_require__.t = function(value, mode) {
+/******/ 			if(mode & 1) value = this(value);
+/******/ 			if(mode & 8) return value;
+/******/ 			if(typeof value === 'object' && value) {
+/******/ 				if((mode & 4) && value.__esModule) return value;
+/******/ 				if((mode & 16) && typeof value.then === 'function') return value;
+/******/ 			}
+/******/ 			var ns = Object.create(null);
+/******/ 			__webpack_require__.r(ns);
+/******/ 			var def = {};
+/******/ 			leafPrototypes = leafPrototypes || [null, getProto({}), getProto([]), getProto(getProto)];
+/******/ 			for(var current = mode & 2 && value; typeof current == 'object' && !~leafPrototypes.indexOf(current); current = getProto(current)) {
+/******/ 				Object.getOwnPropertyNames(current).forEach((key) => (def[key] = () => (value[key])));
+/******/ 			}
+/******/ 			def['default'] = () => (value);
+/******/ 			__webpack_require__.d(ns, def);
+/******/ 			return ns;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./registry/lib/components/video/player/auto-light/index.ts");
+/******/ 	__webpack_exports__ = __webpack_exports__.component;
+/******/ 	
+/******/ 	return __webpack_exports__;
+/******/ })()
+;
+});
--- registry/lib/components/video/player/auto-light/index.ts
+++ registry/lib/components/video/player/auto-light/index.ts
@@ -1,9 +1,75 @@
 import { defineComponentMetadata } from '@/components/define'
 import { playerAgent } from '@/components/video/player-agent'
+import { lightOn, lightOff } from '@/components/video/player-light';
 import { videoChange } from '@/core/observer'
 import { allVideoUrls } from '@/core/utils/urls'
 
-let playerAgentInstance
+let playerAgentInstance;
+
+const CreateAnim = () => {
+  let biliMainHeader = document.getElementById("biliMainHeader");
+  if (!biliMainHeader) return;
+  let mstars1 = document.createElement("div");
+  mstars1.id = "mstars1";
+  let mstars2 = document.createElement("div");
+  mstars2.id = "mstars2";
+  biliMainHeader.appendChild(mstars1);
+  biliMainHeader.appendChild(mstars2);
+
+  // 添加一段css 样式到document最后
+  let style = document.createElement("style");
+  // generate random stars
+  function generate(numCtrl) {
+    let star = '';
+    let max = window.innerWidth * window.innerHeight;
+    for (let i = 0; i < max / numCtrl; i++) {
+      star += `${Math.floor(Math.random() * window.innerWidth * 1.5)}px ${Math.floor(Math.random() * (window.innerHeight + 2000))}px #FFF,`
+    }
+    star += `${Math.floor(Math.random() * window.innerWidth * 1.5)}px ${Math.floor(Math.random() * (window.innerHeight + 2000))}px #FFF;`
+    return star;
+  }
+  let starNumCtl = 400;
+  style.innerHTML = `
+  #mstars1{z-index: 1009;position: fixed;left:0px; width:1px;height:1px;background:transparent;box-shadow:${generate(starNumCtl)};animation:animStar 50s linear infinite}
+  #mstars1:after{content:" ";position:fixed;left:0px;top:0px;width:1px;height:1px;background:transparent;box-shadow:${generate(starNumCtl * 2)}}
+  #mstars2{z-index: 1009;position: fixed;left:0px;width:2px;height:2px;background:transparent;box-shadow:${generate(starNumCtl * 4)};animation:animStar 100s linear infinite}
+  #mstars2:after{content:" ";position:fixed;left:0px;top:0px;width:2px;height:2px;background:transparent;box-shadow:${generate(starNumCtl * 8)}}
+  @keyframes animStar{from{transform:translateY(${-200}px)}to{transform:translateY(${-2200}px)}}
+  `;
+  document.body.appendChild(style);
+}
+
+const StarAnim = (on: boolean) => {
+  //查找id mstars1 的div
+  let mstars1 = document.getElementById("mstars1");
+  let mstars2 = document.getElementById("mstars2");
+  //如果没有找到id biliMainHeader 的div创建2个id为 mstars1 mstars2 的div
+  if (on) {
+    if (mstars1 == null) {
+      CreateAnim();
+      mstars1 = document.getElementById("mstars1");
+      mstars2 = document.getElementById("mstars2");
+    }
+    //设置mstars1 mstars2 visible 为true
+    mstars1.style.visibility = "visible";
+    mstars2.style.visibility = "visible";
+  } else {
+    if (mstars1 != null) {
+      mstars1.style.visibility = "hidden";
+      mstars2.style.visibility = "hidden";
+    }
+  }
+}
+
+let lightOn2 = () => {
+  lightOn();
+  StarAnim(false);
+}
+
+let lightOff2 = () => {
+  lightOff();
+  StarAnim(true);
+}
 
 export const component = defineComponentMetadata({
   name: 'playerAutoLight',
@@ -24,21 +90,21 @@ export const component = defineComponentMetadata({
     videoChange(async () => {
       if (playerAgentInstance != null) {
         const oldVideo = await playerAgentInstance.query.video.element()
-        oldVideo.removeEventListener('ended', lightOn)
-        oldVideo.removeEventListener('pause', lightOn)
-        oldVideo.removeEventListener('play', lightOff)
+        oldVideo.removeEventListener('ended', lightOn2)
+        oldVideo.removeEventListener('pause', lightOn2)
+        oldVideo.removeEventListener('play', lightOff2)
       }
 
       playerAgentInstance = playerAgent
       const video = await playerAgentInstance.query.video.element()
 
       if (playerAgentInstance.isAutoPlay()) {
-        lightOff()
+        lightOff2()
       }
 
-      video.addEventListener('ended', lightOn)
-      video.addEventListener('pause', lightOn)
-      video.addEventListener('play', lightOff)
+      video.addEventListener('ended', lightOn2)
+      video.addEventListener('pause', lightOn2)
+      video.addEventListener('play', lightOff2)
     })
   },
 })
