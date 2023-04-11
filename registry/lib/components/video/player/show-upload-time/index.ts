6e223fc10d47ea8a5059eaa66d04462fe05d689d
wisokey <57711857+wisokey@users.noreply.github.com>
2023-04-11 18:32:12 +0800

    显示视频投稿时间代码格式更正

--- registry/lib/components/video/player/show-upload-time/index.ts
+++ registry/lib/components/video/player/show-upload-time/index.ts
@@ -43,19 +43,53 @@ export const component = defineComponentMetadata({
   author: { name: 'wisokey', link: 'https://github.com/wisokey' },
   name: 'showUploadTime',
   displayName,
-  description: "为视频播放页面的推荐列表中的视频添加显示视频投稿时间.\r\n\r\n`时间格式` 替换up名的文本格式 (默认为'up · yyyy-MM-dd'):\r\n  - y: 年\r\n  - M: 月\r\n  - d: 日\r\n  - h: 时\r\n  - m: 分\r\n  - s: 秒\r\n  - q: 季度\r\n  - up: up名\r\n",
+  description:
+    "为视频播放页面的推荐列表中的视频添加显示视频投稿时间.\r\n\r\n`时间格式` 替换up名的文本格式 (默认为'up · yyyy-MM-dd'):\r\n  - y: 年\r\n  - M: 月\r\n  - d: 日\r\n  - h: 时\r\n  - m: 分\r\n  - s: 秒\r\n  - q: 季度\r\n  - up: up名\r\n",
   tags: [componentsTags.video],
   urlInclude: videoUrls,
   options: {
     formatString: {
       displayName: '文本格式',
       defaultValue: 'up · yyyy-MM-dd',
-      validator: (value: string, oldValue: string) => !value?.trim() ? oldValue : value,
-    }
+      validator: (value: string, oldValue: string) => (!value?.trim() ? oldValue : value),
+    },
   },
   entry: async ({ metadata }) => {
-    
-    const showUploadTime = (relist: VideoPageCard[], forceUpdate: boolean = false, formatString: string = '') => {
+    const getFormatStr = (time: Date, format: string, upName: string) => {
+      const formatMap: any = {
+        'M+': time.getMonth() + 1, // 月
+        'd+': time.getDate(), // 日
+        'h+': time.getHours(), // 时
+        'm+': time.getMinutes(), // 分
+        's+': time.getSeconds(), // 秒
+        'q+': Math.floor((time.getMonth() + 3) / 3), // 季度
+        up: upName, // up主名
+      }
+      let matchResult: RegExpMatchArray | null = format.match(/(y+)/)
+      if (matchResult !== null) {
+        format = format.replace(
+          matchResult[0],
+          `${time.getFullYear()}`.substring(4 - matchResult[0].length),
+        )
+      }
+      for (const key in formatMap) {
+        if (!key) {
+          continue
+        }
+        matchResult = format.match(new RegExp(`(${key})`))
+        if (matchResult !== null) {
+          format = format.replace(
+            matchResult[0],
+            matchResult[0].length === 1
+              ? formatMap[key]
+              : `00${formatMap[key]}`.substring(`${formatMap[key]}`.length),
+          )
+        }
+      }
+      return format
+    }
+
+    const showUploadTime = (relist: VideoPageCard[], forceUpdate = false, formatString = '') => {
       if (!formatString) {
         const { options } = getComponentSettings(metadata.name)
         formatString = options.formatString?.toString()
@@ -80,7 +114,7 @@ export const component = defineComponentMetadata({
             if (!video.oldname) {
               video.oldname = video.name
             }
-            video.name = getformatStr(createTime, formatString, video.oldname)
+            video.name = getFormatStr(createTime, formatString, video.oldname)
           }
           // 保存生成后的name
           video.item.owner.name = video.name
@@ -102,41 +136,17 @@ export const component = defineComponentMetadata({
       return recoList
     }
 
-    const getformatStr = (time: Date, format: string, upName: string) => {
-      const formatMap: any = {
-        'M+': time.getMonth() + 1, // 月
-        'd+': time.getDate(), // 日
-        'h+': time.getHours(), // 时
-        'm+': time.getMinutes(), // 分
-        's+': time.getSeconds(), // 秒
-        'q+': Math.floor((time.getMonth() + 3) / 3), // 季度
-        'up': upName, // up主名
-      }
-      if (/(y+)/.test(format)) {
-        format = format.replace(
-          RegExp.$1,
-          (time.getFullYear() + '').substr(4 - RegExp.$1.length),
+    addComponentListener(
+      `${metadata.name}.formatString`,
+      (value: string) => {
+        const recoList: RecommendList = getRecoList()
+        const relist: VideoPageCard[] = recoList.$children.filter(
+          video => video.$el.className.indexOf('special') === -1,
         )
-      }
-      for (const key in formatMap) {
-        if (new RegExp(`(${key})`).test(format)) {
-          format = format.replace(
-            RegExp.$1,
-            RegExp.$1.length === 1
-              ? formatMap[key]
-              : ('00' + formatMap[key]).substr(('' + formatMap[key]).length))
-        }
-      }
-      return format
-    }
-
-    addComponentListener(`${metadata.name}.formatString`, (value: string) => {
-      const recoList: RecommendList = getRecoList()
-      const relist: VideoPageCard[] = recoList.$children.filter(
-        video => video.$el.className.indexOf('special') === -1,
-      )
-      showUploadTime(relist, true, value)
-    },false)
+        showUploadTime(relist, true, value)
+      },
+      false,
+    )
 
     urlChange(async () => {
       await playerReady()
