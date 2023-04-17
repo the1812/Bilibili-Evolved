389ff59c8e30a35bac106c5236922b0fd976c3d3
the1812 <the1812@outlook.com>
2023-04-17 09:23:29 +0800

    Add ExtraOptions

---
+++ registry/lib/components/style/home-redesign/fresh/ExtraOptions.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="fresh-home-extra-options"></div>
+</template>
+<script lang="ts">
+export default Vue.extend({})
+</script>
+<style lang="sass">
+.fresh-home-extra-options {
+
+}
+</style>
