14f2760f70f5dc7c5aebd358094282d90ec91107
ZiuChen <chenziyu03@kuaishou.com>
2023-04-27 12:47:02 +0800

    fix: 增加对设置的监听 变化实时反应到页面上

--- registry/dist/components/style/player-on-top-new.js
+++ registry/dist/components/style/player-on-top-new.js
@@ -21,19 +21,24 @@
     const o = coreApis.componentApis.define,
       n = coreApis.settings,
       r = coreApis.utils,
-      i = coreApis.utils.urls
-    function p(e, t) {
+      a = coreApis.utils.urls
+    function i(e, t) {
       const o = e.parentElement
       o && (o.removeChild(e), o.insertBefore(e, t.nextSibling))
     }
-    const a = async e => {
+    const p = async e => {
         let {
           settings: { options: t },
+          metadata: o,
         } = e
         await (0, r.playerReady)()
-        p(document.querySelector('#viewbox_report'), document.querySelector('#arc_toolbar_report'))
-        p(document.querySelector('#v_upinfo').parentElement, document.querySelector('#danmukuBox'))
-        document.querySelector('#playerWrap').style.marginTop = `${t.marginTop}px`
+        i(document.querySelector('#viewbox_report'), document.querySelector('#arc_toolbar_report'))
+        i(document.querySelector('#v_upinfo').parentElement, document.querySelector('#danmukuBox'))
+        const a = document.querySelector('#playerWrap')
+        ;(a.style.marginTop = `${t.marginTop}px`),
+          (0, n.addComponentListener)(`${o.name}.marginTop`, e => {
+            a.style.marginTop = `${e}px`
+          })
       },
       s = (0, o.defineComponentMetadata)({
         name: 'playerOnTopNew',
@@ -49,13 +54,13 @@
             validator: (0, r.getNumberValidator)(0, 1 / 0),
           },
         },
-        urlInclude: i.videoUrls,
+        urlInclude: a.videoUrls,
         displayName: '播放器置顶（新）',
         description:
           '原来的播放器置顶插件，现在已经不可用了，这是一个新的版本，可以在视频页面中将播放器放在页面最上方.',
-        entry: a,
-        reload: () => a({ settings: (0, n.getComponentSettings)('playerOnTopNew') }),
-        commitHash: '8f10496200ab80dba32fa89e9beb48e96cff20fb',
+        entry: p,
+        reload: () => p({ settings: (0, n.getComponentSettings)('playerOnTopNew'), metadata: s }),
+        commitHash: '045a6ac28cc2b6103b26d66b7ba56042ca84954a',
         coreVersion: '2.7.1',
       })
     return (t = t.component)
