c5d75818434050e9bba8211a002f4a920f09674b
the1812 <the1812@outlook.com>
2023-05-28 17:13:56 +0800

    Support watchlater list (#4174)

--- registry/lib/components/style/hide/video/related-videos/index.ts
+++ registry/lib/components/style/hide/video/related-videos/index.ts
@@ -12,9 +12,5 @@ export const component = defineComponentMetadata({
     },
   ],
   tags: [componentsTags.style, componentsTags.video],
-  description: {
-    'zh-CN':
-      '隐藏番剧和视频页面右侧的推荐视频列表. 注意: 如果你想关闭 b 站的自动连播 (自动播放下一个推荐视频) 功能, 需要先取消隐藏视频推荐才能看到开关.',
-  },
   urlInclude: videoAndBangumiUrls,
 })
