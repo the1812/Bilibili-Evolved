257ea6dd30e90ed3ec43e8f92ed366bed2eb7fbc
the1812 <the1812@outlook.com>
2023-05-30 00:00:00 +0800

    Fix sortable not work & add tips

--- registry/lib/components/style/home-redesign/fresh/ExtraOptions.vue
+++ registry/lib/components/style/home-redesign/fresh/ExtraOptions.vue
@@ -2,37 +2,58 @@
   <div class="fresh-home-extra-options">
     <div class="fresh-home-extra-options-title">布局配置</div>
     <VLoading v-if="!loaded" />
+    <div v-if="loaded" class="fresh-home-extra-options-tips">
+      <div class="fresh-home-extra-options-tip">
+        按住并拖动可以调整顺序, 打开换行后下一个元素将会另起一行.
+      </div>
+      <div class="fresh-home-extra-options-legends">
+        <div class="fresh-home-extra-options-legend">
+          <VIcon icon="mdi-arrow-expand-horizontal" :size="16" />
+          <div class="fresh-home-extra-options-legend-text">可变宽度</div>
+        </div>
+        <div class="fresh-home-extra-options-legend">
+          <VIcon icon="mdi-arrow-collapse-horizontal" :size="16" />
+          <div class="fresh-home-extra-options-legend-text">固定宽度</div>
+        </div>
+        <div class="fresh-home-extra-options-legend">
+          <VIcon icon="mdi-wrap" :size="16" />
+          <div class="fresh-home-extra-options-legend-text">切换换行</div>
+        </div>
+      </div>
+    </div>
     <div v-show="loaded" ref="sortList" class="fresh-home-extra-options-sort-list">
       <div
         v-for="item of sortItems"
         :key="item.layoutItem.name"
-        class="fresh-home-extra-options-sort-item-container"
+        :data-name="item.layoutItem.name"
+        class="fresh-home-extra-options-sort-item"
       >
-        <div
-          :data-name="item.layoutItem.name"
-          :style="{ 'flex-grow': item.layoutItem.grow ? '1' : '0' }"
-          class="fresh-home-extra-options-sort-item"
-        >
-          <div class="fresh-home-extra-options-sort-item-name">
-            <VIcon v-if="item.layoutItem.grow" icon="mdi-arrow-expand-horizontal" :size="16" />
-            <VIcon v-else icon="mdi-arrow-collapse-horizontal" :size="16" />
-            {{ item.layoutItem.displayName }}
-          </div>
-          <div class="fresh-home-extra-options-sort-item-config">
-            <VButton
-              icon
-              round
-              @click="item.layoutSettings.linebreak = !item.layoutSettings.linebreak"
-            >
-              <VIcon v-if="item.layoutSettings.linebreak" icon="mdi-wrap" :size="16" />
-              <VIcon v-else icon="mdi-wrap-disabled" :size="16" />
-            </VButton>
-          </div>
+        <VIcon
+          v-if="item.layoutItem.grow"
+          title="可变宽度"
+          icon="mdi-arrow-expand-horizontal"
+          class="fresh-home-extra-options-sort-item-type"
+          :size="16"
+        />
+        <VIcon
+          v-else
+          icon="mdi-arrow-collapse-horizontal"
+          class="fresh-home-extra-options-sort-item-type"
+          title="固定宽度"
+          :size="16"
+        />
+        <div class="fresh-home-extra-options-sort-item-name">{{ item.layoutItem.displayName }}</div>
+        <div class="fresh-home-extra-options-sort-item-actions">
+          <VButton
+            type="transparent"
+            title="换行"
+            icon
+            @click="item.layoutSettings.linebreak = !item.layoutSettings.linebreak"
+          >
+            <VIcon v-if="item.layoutSettings.linebreak" icon="mdi-wrap" :size="16" />
+            <VIcon v-else icon="mdi-wrap-disabled" :size="16" />
+          </VButton>
         </div>
-        <div
-          v-if="item.layoutSettings.linebreak"
-          class="fresh-home-extra-options-sort-item-linebreak"
-        ></div>
       </div>
     </div>
   </div>
@@ -98,30 +119,52 @@ export default Vue.extend({
 .fresh-home-extra-options {
   @include v-stretch(10px);
   padding: 4px 0;
-  &-sort-list {
-    @include h-stretch();
-    column-gap: 8px;
-    row-gap: 4px;
+
+  &-tips {
+    @include v-stretch(4px);
+  }
+  &-tip {
+    opacity: 0.6;
+  }
+  &-legends {
+    @include h-center(8px);
     flex-wrap: wrap;
   }
-  &-sort-item-container {
-    display: contents;
+  &-legend {
+    @include h-center(4px);
+  }
+  .mdi-arrow-expand-horizontal {
+    &,
+    & ~ .fresh-home-extra-options-legend-text {
+      color: #a875ff;
+    }
+  }
+  .mdi-arrow-collapse-horizontal {
+    &,
+    & ~ .fresh-home-extra-options-legend-text {
+      color: #009fd9;
+    }
+  }
+  &-sort-list {
+    @include h-stretch();
+    gap: 6px;
+    flex-wrap: wrap;
   }
   &-sort-item {
-    @include v-center(6px);
+    @include h-center(4px);
     @include border-card();
-    padding: 8px;
+    @include shadow();
+    padding: 2px 4px 2px 8px;
     transition: border-color 0.2s ease-out;
     body.dark &:hover,
     &:hover {
       border-color: var(--theme-color);
       cursor: move;
     }
-    &-name {
-      @include h-center(4px);
-    }
-    &-linebreak {
-      flex: 1 0 100%;
+    &-actions {
+      @include h-center();
+      flex-grow: 1;
+      justify-content: end;
     }
   }
 }
