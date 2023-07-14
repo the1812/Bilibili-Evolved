0b89c2372c89d5d0c4172c3e7a722798a6b41790
TimmyOVO <timmyovo@qq.com>
2023-07-14 09:32:48 +0800

    修复隐藏礼物栏后造成的弹幕发送元素靠上的异常偏移

--- registry/lib/components/live/hide-gift-fullscreen/index.ts
+++ registry/lib/components/live/hide-gift-fullscreen/index.ts
@@ -1,19 +1,22 @@
 import { defineComponentMetadata } from '@/components/define'
 import { liveUrls } from '@/core/utils/urls'
 import { waitForControlBar } from '@/components/live/live-control-bar'
-import { addStyle, removeStyle } from '@/core/style'
+import { addStyle } from '@/core/style'
 
 export const component = defineComponentMetadata({
   name: 'hide-fullscreen-gift-bar',
-  displayName: '隐藏直播时全屏的送礼物选项',
-  description: '隐藏直播的全屏视频播放器的礼物栏',
+  displayName: '全屏直播简化',
+  description: '移除全屏观看直播时的底部礼物栏',
   entry: async () => {
     waitForControlBar({
       callback: async () => {
-        removeStyle('hide-fullscreen-gift')
         addStyle(
           '#full-screen-interactive-wrap { display: none !important }',
           'hide-fullscreen-gift',
+        )
+        addStyle(
+          '#fullscreen-danmaku-vm .fullscreen-danmaku { bottom: 5px !important }',
+          'fix_fullscreen_danmaku',
         )
       },
     })
