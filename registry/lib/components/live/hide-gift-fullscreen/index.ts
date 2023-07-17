4d6ed27de33b75d68ba8600d9c95cf7c69568e31
TimmyOVO <timmyovo@qq.com>
2023-07-17 20:13:50 +0800

    修改使用instantStyles实现 删除编译产物

--- registry/lib/components/live/hide-gift-fullscreen/index.ts
+++ registry/lib/components/live/hide-gift-fullscreen/index.ts
@@ -1,26 +1,17 @@
 import { defineComponentMetadata } from '@/components/define'
 import { liveUrls } from '@/core/utils/urls'
-import { waitForControlBar } from '@/components/live/live-control-bar'
-import { addStyle } from '@/core/style'
 
 export const component = defineComponentMetadata({
   name: 'hide-fullscreen-gift-bar',
-  displayName: '全屏直播简化',
+  displayName: '全屏直播礼物简化',
   description: '移除全屏观看直播时的底部礼物栏',
-  entry: async () => {
-    waitForControlBar({
-      callback: async () => {
-        addStyle(
-          '#full-screen-interactive-wrap { display: none !important }',
-          'hide-fullscreen-gift',
-        )
-        addStyle(
-          '#fullscreen-danmaku-vm .fullscreen-danmaku { bottom: 5px !important }',
-          'fix_fullscreen_danmaku',
-        )
-      },
-    })
-  },
-  tags: [componentsTags.live],
+  instantStyles: [
+    {
+      name: 'hide-fullscreen-gift',
+      style: () => import('./hide-full-screen-gift.css'),
+    },
+  ],
+  entry: none,
+  tags: [componentsTags.live, componentsTags.style],
   urlInclude: liveUrls,
 })
