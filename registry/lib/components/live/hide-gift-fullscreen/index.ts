0ac3fde2a0e5006c9147444f36e456cf712b3455
TimmyOVO <timmyovo@qq.com>
2023-07-18 19:06:01 +0800

    添加作者信息

--- registry/lib/components/live/hide-gift-fullscreen/index.ts
+++ registry/lib/components/live/hide-gift-fullscreen/index.ts
@@ -5,6 +5,10 @@ export const component = defineComponentMetadata({
   name: 'hide-fullscreen-gift-bar',
   displayName: '全屏直播礼物简化',
   description: '移除全屏观看直播时的底部礼物栏',
+  author: {
+    name: 'TimmyOVO',
+    link: 'https://github.com/TimmyOVO',
+  },
   instantStyles: [
     {
       name: 'hide-fullscreen-gift',
