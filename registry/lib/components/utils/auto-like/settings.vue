e08e7e5409355655ce80e2fc9651740f79dc1486
CrazyboyQCD <yzc13697716338@gmail.com>
2023-07-22 01:03:10 +0800

    因为无法对未加载动态点赞, 对自动点赞添加手动触发, 同时从屏蔽黑名单up主功能中借鉴了黑名单功能并添加

---
+++ registry/lib/components/utils/auto-like/settings.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <div class="custom-black-list-extra-options">
+      <VButton v-if="login" ref="button" @mouseover="setblackListProps" @click="toggleblackList">
+        黑名单
+        <VIcon icon="right-arrow" :size="16" />
+      </VButton>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { getUID } from '@/core/utils'
+import { VIcon, VButton } from '@/ui'
+
+import { loadblackList, setblackListProps, toggleblackList } from './vm'
+
+export default Vue.extend({
+  components: {
+    VIcon,
+    VButton,
+  },
+  data() {
+    return {
+      isFirstLoad: false,
+      login: Boolean(getUID()),
+    }
+  },
+  methods: {
+    toggle() {
+      this.$refs.popup.toggle()
+    },
+    async setblackListProps() {
+      if (this.isFirstLoad) {
+        return
+      }
+      this.isFirstLoad = await loadblackList()
+      if (this.isFirstLoad) {
+        const triggerButton = this.$refs.button.$el as HTMLElement
+        setblackListProps(triggerButton)
+      }
+    },
+    toggleblackList,
+  },
+})
+</script>
+<style lang="scss">
+.custom-black-list-extra-options {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 20px;
+}
+</style>
