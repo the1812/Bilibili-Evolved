13a2f352783f731ef9d91f06b9d9bc51c8a3558e
RieN7 <zrien7@gmail.com>
2023-09-16 05:32:58 +0800

    fix feeds filter bug

--- registry/lib/components/feeds/filter/pattern.ts
+++ registry/lib/components/feeds/filter/pattern.ts
@@ -1,3 +1,5 @@
+import { FeedsCardType, feedsCardTypes } from '@/components/feeds/api'
+
 const testPattern = (pattern: string, text: string) => {
   if (!pattern || !text) {
     return false
@@ -11,11 +13,17 @@ export interface BlockableCard {
   text: string
   username: string
   // [key: string]: string
+  type: FeedsCardType
+  element: HTMLElement
 }
 export const hasBlockedPattern = (pattern: string, card: BlockableCard) => {
   const upNameMatch = pattern.match(/(.+) up:([^ ]+)/)
   if (upNameMatch) {
     return testPattern(upNameMatch[1], card.text) && testPattern(upNameMatch[2], card.username)
+  }
+  // FIXME: 图文动态的 text 为空，应该是 card 相关的 API 有 bug。这里只做简单的判断
+  if (card.type === feedsCardTypes.textWithImages) {
+    card.text = card.element.innerText
   }
   return testPattern(pattern, card.text)
 }
