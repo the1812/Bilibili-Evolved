6e2ad8a47dc6dd6efda4eb0ecf0d28ced779a8f3
the1812 <the1812@outlook.com>
2024-03-05 23:44:43 +0800

    Add component `legacyFeedsImageViewer` (#4645)

---
+++ registry/lib/components/feeds/legacy-image-viewer/index.ts
@@ -0,0 +1,44 @@
+import { defineComponentMetadata } from '@/components/define'
+import { forEachFeedsCard, getVueData } from '@/components/feeds/api'
+import { feedsUrls } from '@/core/utils/urls'
+
+export const component = defineComponentMetadata({
+  name: 'legacyFeedsImageViewer',
+  displayName: '动态图片平铺展示',
+  tags: [componentsTags.feeds],
+  urlInclude: feedsUrls,
+  entry: () => {
+    forEachFeedsCard({
+      added: card => {
+        const vueData = getVueData(card.element)
+
+        // 普通动态
+        const path = 'data.modules.module_dynamic.major.opus.style'
+        const imageViewerStyle: number | null = lodash.get(vueData, path, null)
+        console.log({ imageViewerStyle })
+        if (imageViewerStyle === 1) {
+          lodash.set(vueData, path, undefined)
+          return
+        }
+
+        // 动态详情
+        const modules: any[] = vueData?.data?.modules ?? []
+        const moduleTop = modules.find(m => m.module_type === 'MODULE_TYPE_TOP')
+        const moduleContent = modules.find(m => m.module_type === 'MODULE_TYPE_CONTENT')
+        const album = moduleTop?.module_top?.display?.album
+        const paragraphs: any[] = moduleContent?.module_content?.paragraphs
+        if (album && paragraphs) {
+          modules.splice(modules.indexOf(moduleTop), 1)
+          paragraphs.push({
+            align: 0,
+            para_type: 2,
+            pic: {
+              pics: album.pics,
+              style: 1,
+            },
+          })
+        }
+      },
+    })
+  },
+})
