82c2a6ce624c4a1cd19636f213abfbe88fb5169e
Liki4 <sw1tch@vidar.club>
2024-07-06 01:23:22 +0800

    feature: live-remove-mask-panel

---
+++ registry/lib/components/live/remove-mask-panel/index.ts
@@ -0,0 +1,36 @@
+import { defineComponentMetadata } from '@/components/define'
+import { liveUrls } from '@/core/utils/urls'
+
+const id = 'web-player-module-area-mask-panel'
+const entry = async () => {
+  const observer = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+      mutation.addedNodes.forEach((node) => {
+        if ((node as Element).id === id) {
+          node.parentNode?.removeChild(node);
+          observer.disconnect();
+        }
+      });
+    });
+  });
+
+  const config = { childList: true, subtree: true };
+  const targetNode = document.body;
+  observer.observe(targetNode, config);
+}
+
+export const component = defineComponentMetadata({
+  name: 'removeLiveMaskPanel',
+  displayName: '删除直播马赛克遮罩',
+  author: {
+    name: 'Liki4',
+    link: 'https://github.com/Liki4',
+  },
+  tags: [componentsTags.live, componentsTags.style],
+  description: {
+    'zh-CN': '删除观看直播时某些分区的马赛克遮罩.',
+  },
+  entry,
+  reload: entry,
+  urlInclude: liveUrls,
+})
