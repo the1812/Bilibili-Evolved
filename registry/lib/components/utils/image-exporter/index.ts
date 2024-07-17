7fc04a2d23a9b388882e77e4cf35629326982b12
the1812 <the1812@outlook.com>
2024-07-17 21:20:37 +0800

    Add multiline options

--- registry/lib/components/utils/image-exporter/index.ts
+++ registry/lib/components/utils/image-exporter/index.ts
@@ -11,10 +11,12 @@ const options = defineOptionsMetadata({
   columnFormat: {
     defaultValue: '[title][ - n]',
     displayName: '专栏图片命名格式',
+    multiline: true,
   },
   feedFormat: {
     defaultValue: '[user][ - id][ - n]',
     displayName: '动态图片命名格式',
+    multiline: true,
   },
 })
 
