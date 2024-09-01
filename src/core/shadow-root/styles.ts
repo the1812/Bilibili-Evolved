5daf767966bbab8e86fcbafd5604a19638573d3e
the1812 <the1812@outlook.com>
2024-09-01 21:05:09 +0800

    Add ShadowRootStyleEntry

--- src/core/shadow-root/styles.ts
+++ src/core/shadow-root/styles.ts
@@ -1,15 +1,20 @@
 import { deleteValue, getRandomId } from '../utils'
 import { ShadowDomObserver, shadowDomObserver } from './dom-observer'
 
+interface ShadowRootStyleEntry {
+  id: string
+  styleSheet: CSSStyleSheet
+  remove: () => void
+}
 export class ShadowRootStyles {
   observer: ShadowDomObserver = shadowDomObserver
-  protected stylesMap = new Map<string, CSSStyleSheet>()
+  protected stylesMap = new Map<string, ShadowRootStyleEntry>()
 
-  protected addStyleRecord(id: string, style: CSSStyleSheet) {
-    this.stylesMap.set(id, style)
+  protected addEntry(id: string, entry: ShadowRootStyleEntry) {
+    this.stylesMap.set(id, entry)
   }
 
-  protected removeStyleRecord(id: string) {
+  protected removeEntry(id: string) {
     if (this.stylesMap.has(id)) {
       this.stylesMap.delete(id)
     }
@@ -18,20 +23,26 @@ export class ShadowRootStyles {
   async addStyle(text: string) {
     this.observer.observe()
     const id = `shadow-dom-style-${getRandomId()}`
-    const sheet = new CSSStyleSheet()
-    await sheet.replace(text)
-    document.adoptedStyleSheets.push(sheet)
-    this.addStyleRecord(id, sheet)
+    const styleSheet = new CSSStyleSheet()
+    await styleSheet.replace(text)
+    document.adoptedStyleSheets.push(styleSheet)
 
     const destroy = this.observer.watchShadowDom({
       added: async shadowDom => {
-        shadowDom.shadowRoot.adoptedStyleSheets.push(sheet)
+        shadowDom.shadowRoot.adoptedStyleSheets.push(styleSheet)
       },
     })
-    return () => {
-      destroy()
-      deleteValue(document.adoptedStyleSheets, it => it === sheet)
-      this.removeStyleRecord(id)
+
+    const entry: ShadowRootStyleEntry = {
+      id,
+      styleSheet,
+      remove: () => {
+        destroy()
+        deleteValue(document.adoptedStyleSheets, it => it === styleSheet)
+        this.removeEntry(id)
+      },
     }
+    this.addEntry(id, entry)
+    return entry
   }
 }
