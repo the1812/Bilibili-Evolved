392f696cdc32517a49f1e1e12a1d0279367cca42
the1812 <the1812@outlook.com>
2024-09-08 21:02:01 +0800

    Add original images for articles (#2868)

--- registry/lib/components/utils/image-resolution/resolution.ts
+++ registry/lib/components/utils/image-resolution/resolution.ts
@@ -17,6 +17,7 @@ const widthAndHeightSelectors = [
   // https://github.com/the1812/Bilibili-Evolved/issues/4480
   '.logo-img',
 ]
+const originalImageInArticlesSelectors = ['.article-detail .article-content img']
 
 const walk = (rootElement: Node, action: (node: HTMLElement) => void) => {
   const walker = document.createNodeIterator(rootElement, NodeFilter.SHOW_ELEMENT)
@@ -26,12 +27,21 @@ const walk = (rootElement: Node, action: (node: HTMLElement) => void) => {
     node = walker.nextNode()
   }
 }
+
+interface ImageResolutionHandler {
+  getWidth: (width: number, element: HTMLElement) => number
+  getHeight: (height: number, element: HTMLElement) => number
+}
+
 /**
  * 从开始元素`element`向下遍历所有子节点, 更换其中的图片URL至目标DPI
  * @param dpi 目标DPI
  * @param element 开始元素
  */
-export const imageResolution = async (dpi: number, element: HTMLElement) => {
+export const imageResolution = async (
+  element: HTMLElement,
+  resolutionHandler: ImageResolutionHandler,
+) => {
   const { attributes } = await import('@/core/observer')
   const replaceSource = (
     getValue: (e: HTMLElement) => string | null,
@@ -74,15 +84,24 @@ export const imageResolution = async (dpi: number, element: HTMLElement) => {
       }
     }
 
+    const getReplacedValue = (newWidth: number, newHeight?: number) => {
+      if (newWidth === Infinity || newHeight === Infinity) {
+        return value.replace(resizeRegex, '@')
+      }
+      if (newHeight === undefined) {
+        return value.replace(resizeRegex, `@${newWidth}w`)
+      }
+      return value.replace(resizeRegex, `@${newWidth}w_${newHeight}h`)
+    }
     if (currentHeight !== undefined) {
-      const newWidth = Math.round(dpi * parseInt(currentWidth)).toString()
-      const newHeight = Math.round(dpi * parseInt(currentHeight)).toString()
-      element.setAttribute('data-resolution-width', newWidth)
-      setValue(element, value.replace(resizeRegex, `@${newWidth}w_${newHeight}h`))
+      const newWidth = resolutionHandler.getWidth(parseInt(currentWidth), element)
+      const newHeight = resolutionHandler.getHeight(parseInt(currentHeight), element)
+      element.setAttribute('data-resolution-width', newWidth.toString())
+      setValue(element, getReplacedValue(newWidth, newHeight))
     } else {
-      const newWidth = Math.round(dpi * parseInt(currentWidth)).toString()
-      element.setAttribute('data-resolution-width', newWidth)
-      setValue(element, value.replace(resizeRegex, `@${newWidth}w`))
+      const newWidth = resolutionHandler.getWidth(parseInt(currentWidth), element)
+      element.setAttribute('data-resolution-width', newWidth.toString())
+      setValue(element, getReplacedValue(newWidth))
     }
   }
   attributes(element, () => {
@@ -108,14 +127,35 @@ export const startResolution = styledComponentEntry<Options>(
       settings.options.scale === 'auto'
         ? window.devicePixelRatio
         : parseFloat(settings.options.scale)
-    walk(document.body, it => imageResolution(dpi, it))
+    const handleResolution: ImageResolutionHandler = {
+      getWidth: (currentWidth, element) => {
+        if (
+          settings.options.originalImageInArticles &&
+          originalImageInArticlesSelectors.some(selector => element.matches(selector))
+        ) {
+          return Infinity
+        }
+        return Math.round(dpi * currentWidth)
+      },
+      getHeight: (currentHeight, element) => {
+        if (
+          settings.options.originalImageInArticles &&
+          originalImageInArticlesSelectors.some(selector => element.matches(selector))
+        ) {
+          return Infinity
+        }
+        return Math.round(dpi * currentHeight)
+      },
+    }
+
+    walk(document.body, it => imageResolution(it, handleResolution))
     allMutations(records => {
       records.forEach(record =>
         record.addedNodes.forEach(node => {
           if (node instanceof HTMLElement) {
-            imageResolution(dpi, node)
+            imageResolution(node, handleResolution)
             if (node.nodeName.toUpperCase() !== 'IMG') {
-              walk(node, it => imageResolution(dpi, it))
+              walk(node, it => imageResolution(it, handleResolution))
             }
           }
         }),
