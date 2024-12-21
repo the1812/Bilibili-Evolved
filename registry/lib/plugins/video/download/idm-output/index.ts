746696d53ccdc262c61f3e3a66cf15d47513b70f
the1812 <the1812@outlook.com>
2024-12-21 21:27:50 +0800

    Update description (fix #5062)

--- registry/lib/plugins/video/download/idm-output/index.ts
+++ registry/lib/plugins/video/download/idm-output/index.ts
@@ -7,14 +7,14 @@ import { DownloadVideoOutput } from '../../../../components/video/download/types
 export const plugin: PluginMetadata = {
   name: 'downloadVideo.outputs.idm',
   displayName: '下载视频 - IDM 输出支持',
-  description:
-    '为下载视频增加 IDM 输出支持，建议配合 [ef2.exe](https://github.com/MotooriKashin/ef2) 以简化操作、保留文件名.',
+  description: '为下载视频增加 IDM 输出支持.',
   setup: ({ addData }) => {
     addData('downloadVideo.outputs', (outputs: DownloadVideoOutput[]) => {
       outputs.push({
         name: 'idm',
         displayName: 'IDM',
-        description: '使用 IDM 的 .ef2 格式导出, 可以在 IDM 中使用导入开始下载.',
+        description:
+          '使用 IDM 的 .ef2 格式导出, 可以在 IDM 中使用导入开始下载, 建议配合 [ef2.exe](https://github.com/MotooriKashin/ef2) 以简化操作、保留文件名.',
         runAction: async action => {
           const { infos } = action
           const referer = document.URL.replace(window.location.search, '')
