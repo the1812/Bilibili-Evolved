58b8a631578f8e07a71f8d0199b1a965ca94a98a
magicFeirl <2100709458@qq.com>
2025-02-12 15:36:06 +0800

    feat: 添加隐藏话题面板组件

---
+++ registry/lib/components/style/hide/topic-panel/index.ts
@@ -0,0 +1,14 @@
+import { defineComponentMetadata } from '@/components/define'
+
+export const component = defineComponentMetadata({
+  name: 'hideTopicPanel',
+  displayName: '隐藏话题面板',
+  tags: [componentsTags.style],
+  instantStyles: [
+    {
+      name: 'hideTopicPanel',
+      style: () => import('./hide-topic-panel.scss'),
+    },
+  ],
+  entry: async () => {},
+})
