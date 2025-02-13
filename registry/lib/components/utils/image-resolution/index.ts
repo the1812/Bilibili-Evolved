234d786dcc12e27e91c71159d490142a67edfdb1
the1812 <the1812@outlook.com>
2025-02-13 23:33:14 +0800

    Adjust scale level (fix #5103)

--- registry/lib/components/utils/image-resolution/index.ts
+++ registry/lib/components/utils/image-resolution/index.ts
@@ -9,7 +9,6 @@ const options = defineOptionsMetadata({
   scale: {
     displayName: '缩放级别',
     defaultValue: 'auto',
-    hidden: true,
   },
   originalImageInArticles: {
     displayName: '在专栏中请求原图',
@@ -25,9 +24,5 @@ export const component = defineComponentMetadata({
   tags: [componentsTags.utils],
   enabledByDefault: window.devicePixelRatio > 1,
   entry: startResolution,
-  description: {
-    'zh-CN':
-      '根据屏幕 DPI 请求更高分辨率的图片, 例如 DPI 缩放 200% 则请求 2 倍的分辨率, 加载时间也会相应变长一些. (也会导致某些浏览器里出现图片闪动, 因为本质上是更换了图片源)',
-  },
   options,
 })
