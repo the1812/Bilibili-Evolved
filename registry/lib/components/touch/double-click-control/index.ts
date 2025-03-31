4cac416c94ade478a97897e342c8afb6b548e45c
the1812 <the1812@outlook.com>
2025-03-31 23:07:49 +0800

    Add disableDoubleClickFullscreen (#5095)

--- registry/lib/components/touch/double-click-control/index.ts
+++ registry/lib/components/touch/double-click-control/index.ts
@@ -27,10 +27,6 @@ const entry = async () => {
 export const component = defineComponentMetadata({
   name: 'doubleClickControl',
   displayName: '启用双击控制',
-  description: {
-    'zh-CN':
-      '将视频播放器的操作方式更改为: 单击显示 / 隐藏控制栏, 双击播放 / 暂停. 请注意不能和 `双击全屏` 一同使用.',
-  },
   tags: [componentsTags.touch],
   enabledByDefault: navigator.maxTouchPoints > 0,
   urlInclude: playerUrls,
