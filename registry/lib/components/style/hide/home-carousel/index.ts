368cf8dd959de613be7264116e93d717167ad6c9
wsgh0202 <wsotl7102@outlook.com>
2025-06-30 23:21:20 +0800

    fix: 隐藏首页轮播图失效 feat: 添加保留占位选项

--- registry/lib/components/style/hide/home-carousel/index.ts
+++ registry/lib/components/style/hide/home-carousel/index.ts
@@ -1,15 +1,27 @@
 import { defineComponentMetadata } from '@/components/define'
+import { ComponentEntry } from '@/components/types'
+import { select } from '@/core/spin-query'
+
+const entry: ComponentEntry = async ({ settings }) => {
+  select('.recommended-swipe').then((e: HTMLElement) => {
+    if (settings.options.keepPlaceholder) {
+      e.style.opacity = '0'
+    } else {
+      e.style.display = 'none'
+    }
+  })
+}
 
 export const component = defineComponentMetadata({
   name: 'hideHomeCarousel',
   displayName: '隐藏首页轮播图',
-  entry: none,
+  entry,
   tags: [componentsTags.style],
   urlInclude: [/^https:\/\/www\.bilibili\.com\/$/, /^https:\/\/www\.bilibili\.com\/index\.html$/],
-  instantStyles: [
-    {
-      name: 'hide-home-carousel',
-      style: () => import('./hide-home-carousel.scss'),
+  options: {
+    keepPlaceholder: {
+      displayName: '保留占位',
+      defaultValue: false,
     },
-  ],
+  },
 })
