31d4fbb15ef6ea413e7937dcbfec77746a903058
wsgh0202 <wsotl7102@outlook.com>
2025-07-12 02:11:18 +0800

    feat: 添加隐藏图片、标题选项

--- registry/lib/components/style/hide/home-carousel/index.ts
+++ registry/lib/components/style/hide/home-carousel/index.ts
@@ -28,11 +28,19 @@ export const component = defineComponentMetadata({
   ],
   options: {
     full: {
-      displayName: '隐藏占位',
+      displayName: '隐藏轮播区域占位',
       defaultValue: true,
     },
     transparent: {
-      displayName: '透明化',
+      displayName: '透明化轮播区域',
+      defaultValue: false,
+    },
+    picture: {
+      displayName: '隐藏轮播图片',
+      defaultValue: false,
+    },
+    footerText: {
+      displayName: '隐藏图片标题',
       defaultValue: false,
     },
   },
