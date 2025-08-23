f38122634674b67b1800d1dc796f6f3b73b0c3b8
wsgh0202 <wsotl7102@outlook.com>
2025-08-24 00:46:00 +0800

    refactor: 移动导出类型到ts文件以通过类型检查

--- src/ui/OptionRadioGroup.vue
+++ src/ui/OptionRadioGroup.vue
@@ -43,32 +43,7 @@ import CollapsibleContainer from './CollapsibleContainer.vue'
 import { addComponentListener, getComponentSettings } from '@/core/settings'
 import { componentsMap } from '@/components/component'
 import OptionWidgetLayout from './OptionWidgetLayout.vue'
-
-export type RadioItem = {
-  /**
-   * 选项名称
-   *
-   * isOption为false时直接显示文本
-   * isOption为true时显示对应选项的displayName
-   */
-  name: string
-
-  /** 是否为脚本设置选项 */
-  isOption?: boolean
-
-  /** 下拉面板中包含的选项名称，可以是名称数组或正则表达式 */
-  optionsIncluded?: string[] | RegExp
-
-  /**
-   * 是否选中
-   *
-   * isOption为true时将忽略设置，自动绑定为选项状态
-   */
-  checked?: boolean
-
-  /** 选项变化时的回调 */
-  onChange?: (checked: boolean) => void
-}
+import { RadioItem } from './radioItem'
 
 type RadioItemVM = RadioItem & {
   /** 最终显示名称 */
