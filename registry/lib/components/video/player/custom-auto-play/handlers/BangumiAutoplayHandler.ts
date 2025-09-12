67d6a2a785a556579a0920df6f7eddc746dd263f
wsgh0202 <wsotl7102@outlook.com>
2025-09-13 02:31:03 +0800

    fix: 修复正片为数字列表模式时自动模式失效

--- registry/lib/components/video/player/custom-auto-play/handlers/BangumiAutoplayHandler.ts
+++ registry/lib/components/video/player/custom-auto-play/handlers/BangumiAutoplayHandler.ts
@@ -11,7 +11,16 @@ export class BangumiAutoplayHandler extends BaseAutoplayHandler {
     return bangumiUrls.some(url => matchUrlPattern(url))
   }
 
+  protected override getSequentialNumberString(): string {
+    return document.querySelector('.plp-r span[class^=eplist_ep_list_progress]').innerHTML
+  }
+
   protected override isLastSequentialNumber(): boolean {
+    // 正片有分P序号，使用默认逻辑
+    if (this.getSequentialNumberString().length > 0) {
+      return super.isLastSequentialNumber()
+    }
+
     // PV、小剧场没有分P序号，因此统一用元素位置判断
     const el = document
       .querySelector('.plp-r img[class^=PlayingIcon_playIcon]')
