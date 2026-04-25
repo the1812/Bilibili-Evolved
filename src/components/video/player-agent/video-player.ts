9939bde38af52e3bc0fcd469c78ad7318e1c1967
the1812 <the1812@outlook.com>
2026-04-25 11:23:09 +0800

    Refactor toggleSubtitle (#5560)

--- src/components/video/player-agent/video-player.ts
+++ src/components/video/player-agent/video-player.ts
@@ -3,7 +3,6 @@ import { PlayerAgent, selectorWrap } from './base'
 import { PlayerQuery, ElementQuery, AgentType } from './types'
 import { select } from '@/core/spin-query'
 import { bpxSelectors } from './bpx'
-import { Toast } from '@/core/toast'
 
 export class VideoPlayerBpxAgent extends PlayerAgent {
   type: AgentType = 'video'
@@ -40,56 +39,5 @@ export class VideoPlayerBpxAgent extends PlayerAgent {
       })
       return func
     })()
-  }
-
-  toggleSubtitle(): HTMLElement {
-    const closeSwitch = dq('.bpx-player-ctrl-subtitle-close-switch') as HTMLDivElement | null
-    // 增加当前视频无字幕功能时的提示
-    if (!closeSwitch) {
-      Toast.info('当前视频没有提供字幕功能', '提示', 5000)
-      return
-    }
-    const isDisabled = closeSwitch?.classList.contains('bpx-state-active')
-    if (!isDisabled) {
-      closeSwitch?.click()
-      return
-    }
-    const subtitleOptions = dqa(
-      '.bpx-player-ctrl-subtitle-major .bpx-player-ctrl-subtitle-language-item',
-    ) as HTMLDivElement[]
-    // 增加当前视频无字幕时的提示
-    if (!subtitleOptions?.length) {
-      Toast.info('当前视频没有字幕可供选择', '提示', 5000)
-      return
-    }
-    const subtitleLanguage = this.getPlayerConfig<null, string>('subtitle.lan', null)
-    if (subtitleLanguage === null) {
-      subtitleOptions.at(0)?.click()
-      return
-    }
-    // 优先选择用过的选项，其次选择与用过的选项相近的选项，最后考虑 AI 生成选项，都不满足则尝试选择可选项第一个
-    if (subtitleOptions.find(it => it.dataset.lan === subtitleLanguage)) {
-      subtitleOptions.find(it => it.dataset.lan === subtitleLanguage)?.click()
-    } else if (
-      subtitleOptions.find(
-        it =>
-          !it.dataset.lan?.startsWith('ai-') &&
-          it.dataset.lan?.includes(subtitleLanguage.split('-')[0]),
-      )
-    ) {
-      subtitleOptions
-        .find(
-          it =>
-            !it.dataset.lan?.startsWith('ai-') &&
-            it.dataset.lan?.includes(subtitleLanguage.split('-')[0]),
-        )
-        ?.click()
-    } else if (
-      subtitleOptions.find(it => it.dataset.lan === `ai-${subtitleLanguage.split('-')[0]}`)
-    ) {
-      subtitleOptions.find(it => it.dataset.lan === `ai-${subtitleLanguage.split('-')[0]}`)?.click()
-    } else {
-      subtitleOptions.at(0)?.click()
-    }
   }
 }
