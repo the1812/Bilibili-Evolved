261892be329bbebcef63dfc95ac42ee60a74da31
oooolcepsed <152772287+cccccccccooool@users.noreply.github.com>
2026-06-04 00:55:10 +0800

    删除 webpack.danmaku.ts

--- registry/lib/components/live/live-danmaku-helper/webpack.danmaku.ts
+++
@@ -1,9 +0,0 @@
-import { buildByEntry } from '../../../../webpack/config'
-
-export default () => [
-  buildByEntry({
-    src: './registry/lib/components/',
-    type: 'component',
-    entry: './registry/lib/components/live/live-danmaku-helper/index.ts',
-  }),
-]
