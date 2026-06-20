44b69bdb018d9203abcb928d5b7c9f3e4cac5d1d
LainIO24 <lainio24@outlook.com>
2026-06-20 12:59:37 +0800

    feat(quickFavorite): rbvp plugin

--- registry/lib/components/video/quick-favorite/index.ts
+++ registry/lib/components/video/quick-favorite/index.ts
@@ -1,11 +1,15 @@
 import { defineComponentMetadata } from '@/components/define'
 import { ComponentEntry } from '@/components/types'
+import { addVideoActionButton } from '@/components/video/video-actions'
+import { videoChange } from '@/core/observer'
 import { getUID, matchUrlPattern, mountVueComponent } from '@/core/utils'
 import { favoriteListUrls, videoUrls } from '@/core/utils/urls'
 import { KeyBindingAction } from '../../utils/keymap/bindings'
-import { addVideoActionButton } from '@/components/video/video-actions'
-import { videoChange } from '@/core/observer'
 import { options, Options } from './options'
+import { rbvpNamespaceProvider } from './rbvp-provider'
+
+export const componentName = 'quickFavorite'
+export const displayName = '快速收藏'
 
 const entry: ComponentEntry<Options> = async ({ settings }) => {
   if (favoriteListUrls.some(matchUrlPattern) && !settings.options.showInFavoritePages) {
@@ -26,7 +30,7 @@ const entry: ComponentEntry<Options> = async ({ settings }) => {
   })
 }
 export const component = defineComponentMetadata({
-  name: 'quickFavorite',
+  name: componentName,
   displayName: '启用快速收藏',
   description: {
     'zh-CN':
@@ -43,21 +47,39 @@ export const component = defineComponentMetadata({
   // urlExclude: favoriteListUrls,
   tags: [componentsTags.video],
   options,
-  plugin: {
-    displayName: '快速收藏 - 快捷键支持',
-    setup: ({ addData }) => {
-      addData('keymap.actions', (actions: Record<string, KeyBindingAction>) => {
-        actions.quickFavorite = {
-          displayName: '快速收藏',
-          run: context => {
-            const { clickElement } = context
-            return clickElement('.be-quick-favorite', context)
-          },
-        }
-      })
-      addData('keymap.presets', (presetBase: Record<string, string>) => {
-        presetBase.quickFavorite = 'shift s'
-      })
+  plugin: [
+    {
+      name: `${componentName}.keymap`,
+      displayName: `${displayName} - 快捷键支持`,
+      setup: ({ addData }) => {
+        addData('keymap.actions', (actions: Record<string, KeyBindingAction>) => {
+          actions.quickFavorite = {
+            displayName,
+            run: context => {
+              const { clickElement } = context
+              return clickElement('.be-quick-favorite', context)
+            },
+          }
+        })
+        addData('keymap.presets', (presetBase: Record<string, string>) => {
+          presetBase.quickFavorite = 'shift s'
+        })
+      },
     },
-  },
+    {
+      name: `${componentName}.rbvp`,
+      displayName: `${displayName} - RBVP 兼容`,
+      author: [
+        {
+          name: 'LainIO24',
+          link: 'https://github.com/LainIO24',
+        },
+      ],
+      setup: ({ addData }) => {
+        addData('rbvp.namespaces', namespaces => {
+          namespaces.quickFavorite = rbvpNamespaceProvider
+        })
+      },
+    },
+  ],
 })
