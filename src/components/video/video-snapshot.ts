76e0785126c6ea9e910f0e2d2a68a51a90080680
WakelessSloth56 <wsloth56@foxmail.com>
2026-06-23 11:17:01 +0800

    feat: `VideoSnapshot` `createGrid` method

--- src/components/video/video-snapshot.ts
+++ src/components/video/video-snapshot.ts
@@ -1,5 +1,6 @@
 /* spellchecker:words pvdata */
 import { bilibiliApi, getJsonWithCredentials } from '@/core/ajax'
+import { formatDuration } from '@/core/utils/formatters'
 
 /**
  * 解析二进制格式的 pvdata
@@ -15,6 +16,8 @@ const parsePVData = async (data: ArrayBuffer) => {
   }
   return result
 }
+
+// =========================================================================== /
 
 export interface SnapshotSprite {
   time: number
@@ -113,9 +116,140 @@ async function splitSnapshots(
 }
 
 /**
+ * 根据时间均匀的取样快照图到指定的数量
+ * @author WakelessSloth56
+ * @param count 需要的快照图数量
+ */
+function sampleByTime<T extends { time: number }>(snapshots: T[], count: number): T[] {
+  if (snapshots.length <= count) {
+    return snapshots
+  }
+
+  const times = snapshots.map(x => x.time)
+  const minTime = times[0]
+  const maxTime = times[times.length - 1]
+  const duration = maxTime - minTime
+
+  const result = []
+  const timeStep = duration / (count - 1)
+
+  for (let i = 0; i < count; i++) {
+    const targetTime = minTime + i * timeStep
+    const closest = snapshots.reduce((prev, curr) => {
+      const prevDiff = Math.abs(prev.time - targetTime)
+      const currDiff = Math.abs(curr.time - targetTime)
+      return currDiff < prevDiff ? curr : prev
+    })
+    result.push(closest)
+  }
+
+  return [...new Set(result)]
+}
+
+// =========================================================================== /
+
+export interface DrawOptions {
+  fontSize?: number
+  font?: string
+  textColor?: string
+  backgroundColor?: string
+  paddingX?: number
+  paddingY?: number
+  marginX?: number
+  marginY?: number
+}
+
+/**
+ * 在快照图上绘制对应的时间点
+ * @author WakelessSloth56
+ */
+function drawTimestamp(snapshot: SnapshotSprite, options: DrawOptions = {}) {
+  const timeStr = formatDuration(snapshot.time)
+  const ctx = snapshot.canvas.getContext('2d')
+
+  const {
+    paddingX = 8,
+    paddingY = 4,
+    marginX = 6,
+    marginY = 3,
+    fontSize = 22,
+    font = 'monospace',
+    textColor = '#ffffff',
+    backgroundColor = '#00000033',
+  } = options
+
+  ctx.font = `${fontSize}px '${font}'`
+  const textW = ctx.measureText(timeStr).width
+
+  const bgW = textW + paddingX * 2
+  const bgH = fontSize + paddingY * 2
+
+  const x = snapshot.canvas.width - bgW - marginX
+  const y = snapshot.canvas.height - bgH - marginY
+
+  ctx.fillStyle = backgroundColor
+  ctx.fillRect(x, y, bgW, bgH)
+
+  ctx.fillStyle = textColor
+  ctx.textBaseline = 'top'
+  ctx.fillText(timeStr, x + paddingX, y + paddingY)
+}
+
+/**
+ * @author WakelessSloth56
+ */
+function createGrid(
+  snapshots: SnapshotSprite[],
+  cols: number,
+  rows: number,
+  spriteWidth: number,
+  spriteHeight: number,
+  options: DrawOptions = {},
+) {
+  const {
+    paddingX = 4,
+    paddingY = 4,
+    marginX = 12,
+    marginY = 12,
+    backgroundColor = '#000000',
+  } = options
+
+  const canvas = document.createElement('canvas')
+  const ctx = canvas.getContext('2d')
+
+  const w = cols * spriteWidth + (cols - 1) * paddingX + marginX * 2
+  const h = rows * spriteHeight + (rows - 1) * paddingY + marginY * 2
+  const x0 = marginX
+  const y0 = marginY
+
+  canvas.width = w
+  canvas.height = h
+
+  ctx.fillStyle = backgroundColor
+  ctx.fillRect(0, 0, w, h)
+
+  for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
+      const i = row * cols + col
+      if (i >= snapshots.length) {
+        return canvas
+      }
+      const x = col * (spriteWidth + paddingX) + x0
+      const y = row * (spriteHeight + paddingY) + y0
+      drawTimestamp(snapshots[i])
+      ctx.drawImage(snapshots[i].canvas, x, y)
+    }
+  }
+  return canvas
+}
+
+// =========================================================================== /
+
+/**
  * @author WakelessSloth56
  */
 export class VideoSnapshot {
+  private ready = false
   aid?: string
   bvid?: string
   cid: number
@@ -168,6 +302,25 @@ export class VideoSnapshot {
       this.spriteHeight,
       toCanvas,
     )
+    this.ready = true
+    console.debug(this)
     return this
+  }
+
+  async createGridWithInfo(cols = 5, rows = 6, options: DrawOptions = {}) {
+    if (!this.ready || !this.snapshots[0].canvas) {
+      throw new Error('[VideoSnapshot] snapshots not ready')
+    }
+
+    const canvas = createGrid(
+      sampleByTime(this.snapshots, cols * rows),
+      cols,
+      rows,
+      this.spriteWidth,
+      this.spriteHeight,
+      options,
+    )
+
+    return canvas
   }
 }
