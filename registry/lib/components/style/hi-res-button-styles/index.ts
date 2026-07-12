18ee4958bd607e9d80e3ea470f809b6a6c11740b
Shoko Mak1nohara <106792493+WhiteTeal55@users.noreply.github.com>
2026-07-12 13:35:37 +0800

    [组件-Hi-Res 音质按钮布局调整] feat: 添加组件选项可设置开启 Hi-Res 时将按钮填充为主题色

--- registry/lib/components/style/hi-res-button-styles/index.ts
+++ registry/lib/components/style/hi-res-button-styles/index.ts
@@ -1,16 +1,38 @@
 import { defineComponentMetadata } from '@/components/define'
+import { addComponentListener } from '@/core/settings'
 import { playerUrls } from '@/core/utils/urls'
 
 export const component = defineComponentMetadata({
   name: 'hiResButtonStyles',
   displayName: 'Hi-Res 音质按钮布局调整',
-  entry: none,
+  entry: () => {
+    addComponentListener(
+      'hiResButtonStyles.fillThemeColor',
+      (value: boolean) => {
+        if (value) {
+          document.documentElement.style.setProperty(
+            '--be-color-button-hires',
+            'var(--theme-color, #00a1d6)',
+          )
+        } else {
+          document.documentElement.style.removeProperty('--be-color-button-hires')
+        }
+      },
+      true,
+    )
+  },
   instantStyles: [
     {
       name: 'hiResButtonStyles',
       style: () => import('./hi-res-button-styles.scss'),
     },
   ],
+  options: {
+    fillThemeColor: {
+      displayName: '开启 Hi-Res 时将按钮填充为主题色',
+      defaultValue: true,
+    },
+  },
   tags: [componentsTags.style, componentsTags.video],
   urlInclude: playerUrls,
 })
