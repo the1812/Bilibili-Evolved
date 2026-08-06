1bfcee772759aac55dcebc9540753ca2d8262234
Shoko Mak1nohara <106792493+WhiteTeal55@users.noreply.github.com>
2026-08-06 10:08:40 +0800

    添加 author 字段

--- registry/lib/components/utils/collection-link-redirect/index.ts
+++ registry/lib/components/utils/collection-link-redirect/index.ts
@@ -4,6 +4,10 @@ export const component = defineComponentMetadata({
   name: 'collectionLinkRedirect',
   displayName: '合集链接重定向',
   tags: [componentsTags.utils],
+  author: {
+    name: 'WhiteTeal55',
+    link: 'https://github.com/WhiteTeal55',
+  },
   entry: () => {
     const searchParams = new URLSearchParams(location.search)
     const sid = searchParams.get('sid')
