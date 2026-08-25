c0bf1c69fc5d2d4d87369ecde73ad0fecc1c9354
WhiteTeal55 <106792493+WhiteTeal55@users.noreply.github.com>
2026-08-26 01:54:13 +0800

    [新增组件] 直播自动网页全屏

---
+++ registry/lib/components/live/auto-full-win/index.ts
@@ -0,0 +1,25 @@
+import { defineComponentMetadata } from '@/components/define'
+import { liveUrls } from '@/core/utils/urls'
+import { select } from '@/core/spin-query'
+
+const isPlayerReady = (player: any) => player?.getPlayerInfo?.()?.playurl && player.switchQuality
+
+export const component = defineComponentMetadata({
+  name: 'autoFullWin',
+  displayName: '直播自动网页全屏',
+  author: {
+    name: 'WhiteTeal55',
+    link: 'https://github.com/WhiteTeal55',
+  },
+  entry: async () => {
+    const player = await select(() => {
+      const p = unsafeWindow.livePlayer as any
+      return isPlayerReady(p) ? p : null
+    })
+
+    player.setFullscreenStatus(1)
+    console.debug('[直播自动网页全屏] 已切换到网页全屏')
+  },
+  tags: [componentsTags.live],
+  urlInclude: liveUrls,
+})
