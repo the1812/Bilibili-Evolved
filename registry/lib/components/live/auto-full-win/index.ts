86e0f23f87d095bf54e4d2c0bd2ff5f02b3929f5
WhiteTeal55 <106792493+WhiteTeal55@users.noreply.github.com>
2026-08-29 00:09:09 +0800

    新增: 添加选项和滚动到播放器区域的逻辑

--- registry/lib/components/live/auto-full-win/index.ts
+++ registry/lib/components/live/auto-full-win/index.ts
@@ -1,8 +1,21 @@
-import { defineComponentMetadata } from '@/components/define'
+import {
+  defineComponentMetadata,
+  OptionsOfMetadata,
+  defineOptionsMetadata,
+} from '@/components/define'
+
 import { liveUrls } from '@/core/utils/urls'
 import { select } from '@/core/spin-query'
 
 const isPlayerReady = (player: any) => player?.getPlayerInfo?.()?.playurl
+
+const options = defineOptionsMetadata({
+  autoScrollOnStyle: {
+    displayName: '在带有活动样式的直播间自动滚动到播放器区域',
+    defaultValue: true,
+  },
+})
+export type Options = OptionsOfMetadata<typeof options>
 
 export const component = defineComponentMetadata({
   name: 'autoFullWin',
@@ -11,15 +24,22 @@ export const component = defineComponentMetadata({
     name: 'WhiteTeal55',
     link: 'https://github.com/WhiteTeal55',
   },
-  entry: async () => {
+  entry: async ({ settings }) => {
     const player = await select(() => {
       const p = unsafeWindow.livePlayer as any
       return isPlayerReady(p) ? p : null
     })
 
+    const stylePlayerArea = dq('.live-player-bg .player')
+    if (stylePlayerArea && settings.options.autoScrollOnStyle) {
+      stylePlayerArea.scrollIntoView({ block: 'center' })
+      console.debug('[直播自动网页全屏] 已滚动到播放器区域')
+    }
+
     player.setFullscreenStatus(1)
     console.debug('[直播自动网页全屏] 已切换到网页全屏')
   },
+  options,
   tags: [componentsTags.live],
   urlInclude: liveUrls,
 })
